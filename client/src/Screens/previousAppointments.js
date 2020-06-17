248b8fc6c979b3edd737d22f6ca6eebf6233100f
Hashem Abed Al Jawad <dashinghashem@yahoo.com>
2020-06-17 16:40:59 +0300

    Add Compontents & screens

---
+++ client/src/Screens/previousAppointments.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import AppointmentsList from '../components/appointmentsList'
+
+const PreviousAppointments = () => {
+    
+    return (
+
+        <AppointmentsList/>
+
+
+
+    )
+}
+
+export default PreviousAppointments
