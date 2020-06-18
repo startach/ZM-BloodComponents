98c6b73fa53887da8c58ab983dc8e8268fc7c9f8
Jake <jakepowis@gmail.com>
2020-06-18 13:29:27 +0300

    adding sate to questionarre page

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import UserPage from './Components/UserPage/UserPage'
+import Questionnaire from './Components/Questionnaire/Questionnaire'
 
 function App() {
   return (
     <div className="App">
-      <UserPage />
+
+      <Questionnaire />
     </div>
   );
 }
---
+++ client/src/Components/Questionnaire/Questionnaire.js
@@ -0,0 +1,89 @@
+import React, { useState } from 'react'
+import './Questionnaire.css'
+
+export default function Questionnaire() {
+
+    const [result, setResults] = useState({
+        Q1: "select",
+        Q2: "select",
+        Q3: "select",
+        Q4: "select",
+        Q5: "select",
+        Q6: "select",
+        Q7: "select",
+        Q8: "select",
+        Q9: "select",
+        Q10: "select",
+        Q11: "select",
+        Q12: "select",
+        Q13: "select",
+        Q14: "select",
+        Q15: "select",
+        Q16: "select",
+        Q17: "select",
+    })
+
+
+
+    const questionList = [
+        { "id": 1, "question": "Have you ever donated blood or Thrombocytes?", "options": ["Yes", "No"] },
+        { "id": 2, "question": "Is your weight above 55kg?", "options": ["Yes", "No"] },
+        { "id": 3, "question": "Is your weight above 50kg?", "options": ["Yes", "No"] },
+        { "id": 4, "question": "Did you do a tattoo/earrings/piercing in the last 6 months?", "options": ["Yes", "No"] },
+        { "id": 5, "question": "Do you have diabetes", "options": ["Yes, but stable treated by medicines", "Yes, but not stable or treated with Insulin", "No"] },
+        { "id": 6, "question": "Do you take medicines?", "options": ["Yes", "No"] },
+        { "id": 7, "question": "Have you been abroad in the last year?", "options": ["Yes", "No"] },
+        { "id": 8, "question": "Have you gone through a medical surgery in the last month?", "options": ["Yes", "No"] },
+        { "id": 9, "question": "Do you suffer from a chronical disease?", "options": ["Yes", "No"] },
+        { "id": 10, "question": "Have you ever suffered from cancer?", "options": ["Yes", "No"] },
+        { "id": 11, "question": "Did you take antibiotics in the last 3 days?", "options": ["Yes", "No"] },
+        { "id": 12, "question": "Have you gone through Dentist procedure in the last 10 days?", "options": ["Yes", "No"] },
+        { "id": 13, "question": "Do you have an open wound or a scratch?", "options": ["Yes", "No"] },
+        { "id": 14, "question": "Do you confirm your age is between 17 and 65?", "options": ["Yes", "No"] },
+        { "id": 15, "question": "For women only: Have you been pregnant in the last 6 months?", "options": ["Yes", "No"] },
+        { "id": 16, "question": "When was your last donation?", "options": ["Less than one month ago", "Less than one month, more than ten days ago", "Less than 10 days ago", "Never"] },
+        { "id": 17, "question": "Reading and truth statement confirmation", "options": ["I confirm", "I do not confirm"] },
+    ]
+
+    const handleResults = (e, index) => {
+        let thisQ = "Q" + (index + 1);
+        setResults({ ...result, [thisQ]: e.target.value })
+        console.log(result)
+
+    }
+
+
+    return (
+        <div className="questionnairePage">
+
+
+            <div className="title">Questionnaire</div>
+            <div className="line1"></div>
+
+
+            {questionList.map((question, index) => (
+
+                <div className="questions">
+                    <div className="left">
+                        <div><b>{question.id}:</b> {question.question}</div>
+                    </div>
+
+                    <div className="right">
+                        <select class="dropdown" onChange={e => handleResults(e, index)}>
+                            <option disabled="disabled" selected="selected">Select</option>
+                            {question.options.map((option) => <option>{option}</option>)}
+                        </select>
+                    </div>
+
+                </div>
+            )
+
+            )}
+            <div className="submit">
+                <div className="submitBtn" onClick={handleResults}>Submit</div>
+            </div>
+
+
+        </div>
+    )
+}
