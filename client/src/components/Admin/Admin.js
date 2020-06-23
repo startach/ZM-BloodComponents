28fab72a881c65edab147e1d2b57035515068a2f
Jake <jakepowis@gmail.com>
2020-06-23 11:25:06 +0300

    clean up nav bar, and add remove functionality for admins/cords

--- client/src/components/Admin/Admin.js
+++ client/src/components/Admin/Admin.js
@@ -35,7 +35,7 @@ export default function Admin() {
         addAdminRole({ email: control.admin }).then(result => {
             console.log(result);
         });
-        setControl(control.admin = "")
+
     }
 
 
@@ -45,11 +45,22 @@ export default function Admin() {
         e.preventDefault();
 
         const addCordRole = functions.httpsCallable('addCordRole');
-        console.log("cord to add: ", control.cord)
         addCordRole({ email: control.cord }).then(result => {
             console.log(result);
         });
-        setControl(control.cord = "")
+
+
+    }
+
+
+    const handleRemove = (e, role) => {
+        e.preventDefault();
+
+        const removeRole = functions.httpsCallable('removeRole');
+        removeRole({ email: control.role }).then(result => {
+            console.log(result);
+        });
+
 
     }
 
@@ -89,7 +100,7 @@ export default function Admin() {
 
 
 
-            <form className="mb-5" style={{ margin: "40px auto", maxWidth: "300px" }} onSubmit={handleCord}>
+            <form className="mb-5" style={{ margin: "40px auto", maxWidth: "300px" }} >
                 <input type="email" placeholder="User email" id="cord-email" value={control.cord} required />
 
                 <select className="dropdown mt-2" id="cord" onChange={handleChange}>
@@ -101,13 +112,14 @@ export default function Admin() {
                     ))}
                 </select>
 
-                <button class="btn btn-small btn-warning mt-2">Make Cordinator</button>
+                <button class="btn btn-small btn-warning mt-2" onClick={handleCord}>Make Cordinator</button>
+                <button className="btn btn-small btn-secondary mt-2 ml-2" onClick={(e) => handleRemove(e, "cord")}>Remove</button>
             </form>
 
 
 
 
-            <form style={{ margin: "40px auto", maxWidth: "300px" }} onSubmit={handleAdmin}>
+            <form style={{ margin: "40px auto", maxWidth: "300px" }} >
                 <input type="email" placeholder="User email" id="admin-email" value={control.admin} required />
 
                 <select className="dropdown mt-2" id="admin" onChange={handleChange}>
@@ -118,9 +130,10 @@ export default function Admin() {
 
                     ))}
                 </select>
-                <button className="btn btn-small btn-danger mt-2">Make Admin</button>
+                <button className="btn btn-small btn-danger mt-2" onClick={handleAdmin}>Make Admin</button>
+                <button className="btn btn-small btn-secondary mt-2 ml-2" onClick={(e) => handleRemove(e, "admin")}>Remove</button>
             </form>
 
-        </div>
+        </div >
     )
 }
--- functions/index.js
+++ functions/index.js
@@ -38,3 +38,23 @@ exports.addCordRole = functions.https.onCall((data, context) => {
         return err;
     });
 });
+
+
+
+exports.removeRole = functions.https.onCall((data, context) => {
+    // if (context.auth.token.admin !== true) {
+    //     return { error: 'Only admins can add Cordinators' }
+    // }
+
+    return admin.auth().getUserByEmail(data.email).then(user => {
+        return admin.auth().setCustomUserClaims(user.uid, {
+            userLevel: "none",
+        })
+    }).then(() => {
+        return {
+            message: `Success! ${data.email} has had their access removed.`
+        }
+    }).catch(err => {
+        return err;
+    });
+});
