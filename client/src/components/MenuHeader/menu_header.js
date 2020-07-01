b5f059f3a9d0afef646f02b9f4ab73f1245066c9
Jake <jakepowis@gmail.com>
2020-07-01 16:45:20 +0300

    drop down now works accross multiple screens

--- client/src/components/MenuHeader/menu_header.js
+++ client/src/components/MenuHeader/menu_header.js
@@ -73,11 +73,11 @@ const Menu_header = (props) => {
             </span>
 
       
-
       <select style={{width:"50px"}} onChange={handleChange} >
-            <option value='heb'>עִברִית</option>
-            <option value='ara'>عربى</option>
-            <option value='en'>en</option>
+      <option value={i18next.language}>{i18next.language == 'heb' ? 'עִברִית' : i18next.language == 'ara' ? 'عربى' : 'en'}</option>
+            {i18next.language != 'heb' ? <option value='heb'>עִברִית</option> : null}
+            {i18next.language != 'ara' ? <option value='ara'>عربى</option> : null}
+            {i18next.language != 'en' ? <option value='en'>en</option> : null}
     </select>
  
 
