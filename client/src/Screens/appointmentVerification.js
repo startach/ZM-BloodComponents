a817ae2b8d7c83ba8752a967b1f3156e7b6b757d
Morad Abed <morad890@gmail.com>
2020-07-06 13:40:27 +0300

    adding the screens titles

--- client/src/Screens/appointmentVerification.js
+++ client/src/Screens/appointmentVerification.js
@@ -3,16 +3,19 @@ import ScreenContainer from '../components/screen'
 import MenuHeader from '../components/MenuHeader'
 import VerificationList from '../components/verificationList'
 import BottomNavBar from '../components/BottomNavBar/BottomBar'
-
-
+import { useTranslation } from 'react-i18next';
+import i18next from 'i18next';
 
 
 
 const AppointmentVerification = () => {
+const { t } = useTranslation();
+    
+
     return (
 
             <ScreenContainer>
-                <MenuHeader title="Appointment Verification" icon='burger'/>
+                <MenuHeader title={t('screens.verification')} icon='burger'/>
                 <VerificationList />
                 <BottomNavBar />
                 <div className="footer"></div>
