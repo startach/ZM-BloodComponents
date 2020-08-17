145041415221eb6821551edbc274015b0b8d9437
czimer <czimer93@gmail.com>
2020-08-17 17:00:01 +0300

    change firebaseConfig

---
+++ client/src/components/firebase/firebaseConfig.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyC6EDL8gMkZc3GGzGveMqWe5zvAr5DNiL4",
+    authDomain: "blood-components.firebaseapp.com",
+    databaseURL: "https://blood-components.firebaseio.com",
+    projectId: "blood-components",
+    storageBucket: "blood-components.appspot.com",
+    messagingSenderId: "388223113819",
+    appId: "1:388223113819:web:1273570a12add0fedafd7e",
+    measurementId: "G-K6NM078FWD"
+};
