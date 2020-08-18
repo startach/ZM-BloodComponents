2f751f4fcc636e32de2d0eb3762d2846ee06634d
joavdiamant <joav.di@gmail.com>
2020-08-18 22:30:07 +0300

    chenge path import YesNoPopUp from '../../popUp/YesNoPopup/YesNoPopUp';

--- client/src/components/Dashboard/AppointmentsTable/AppointmentsTable.jsx
+++ client/src/components/Dashboard/AppointmentsTable/AppointmentsTable.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { updateAppointment } from '../../../services/appointmentService';
 import { getHospitalLangName } from '../../../services/hospitalService';
-import YesNoPopUp from '../../popUp/YesNoPopUp/YesNoPopup';
+import YesNoPopUp from '../../popUp/YesNoPopup/YesNoPopup';
 import "../dashboard.css";
 
 export default function AppointmentsTable(props) {
