64920a2ddf72fd3f8df9fc973026b6aa45b1b1fd
czimer <czimer93@gmail.com>
2020-08-25 16:43:19 +0300

    config

--- client/src/components/firebase/firebaseConfig.js
+++ client/src/components/firebase/firebaseConfig.js
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyC6EDL8gMkZc3GGzGveMqWe5zvAr5DNiL4",
-    authDomain: "blood-components.firebaseapp.com",
-    databaseURL: "https://blood-components.firebaseio.com",
-    projectId: "blood-components",
-    storageBucket: "blood-components.appspot.com",
-    messagingSenderId: "388223113819",
-    appId: "1:388223113819:web:1273570a12add0fedafd7e",
-    measurementId: "G-K6NM078FWD"
+    apiKey: "AIzaSyBrpaowMgDhMk9m4alrBQcxOdnVDKlZ678",
+    authDomain: "blood-components-9ad48.firebaseapp.com",
+    databaseURL: "https://blood-components-9ad48.firebaseio.com",
+    projectId: "blood-components-9ad48",
+    storageBucket: "blood-components-9ad48.appspot.com",
+    messagingSenderId: "716612101303",
+    appId: "1:716612101303:web:d3282c2202cbbd43aad932",
+    measurementId: "G-4F5B3GB6SZ"
 };
