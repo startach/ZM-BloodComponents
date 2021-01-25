ad0e0a7e217218c1170edfee3a34dbd45f2c2a22
Harel Gliksman <harelglik@gmail.com>
2021-01-25 17:19:57 +0100

    scripts fixes

--- packages/functions/webpack.config.js
+++ packages/functions/webpack.config.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const path = require("path");
 const nodeExternals = require("webpack-node-externals");
 const GeneratePackageJsonPlugin = require("generate-package-json-webpack-plugin");
 
@@ -10,39 +10,37 @@ const basePackage = {
   //   start: "yarn run shell"
   // },
   engines: {
-    node: "8"
-  }
+    node: "8",
+  },
 };
 
 module.exports = {
-  target: 'node',
-  mode: 'production',
-  entry: './src/index.ts',
+  target: "node",
+  mode: "production",
+  entry: "./src/index.ts",
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js', '.json']
+    extensions: [".tsx", ".ts", ".js", ".json"],
   },
   output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, './dist'),
-    libraryTarget: 'commonjs'
+    filename: "index.js",
+    path: path.resolve(__dirname, "./dist"),
+    libraryTarget: "commonjs",
   },
   externals: [
     /^firebase.+$/,
     /^@google.+$/,
     nodeExternals({
-      allowlist: [/^@zm-blood-components/]
-    })
+      allowlist: [/^@zm-blood-components/],
+    }),
   ],
-  plugins: [
-    new GeneratePackageJsonPlugin(basePackage)
-  ],
+  plugins: [new GeneratePackageJsonPlugin(basePackage)],
 };
