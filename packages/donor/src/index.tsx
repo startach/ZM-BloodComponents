8c114569f9d5aa0bdc01c46dc7d399e02e8e7b95
Harel Gliksman <harelglik@gmail.com>
2021-01-31 21:58:44 +0100

    import common in donor

--- packages/donor/src/index.tsx
+++ packages/donor/src/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { Hospital } from "@zm-blood-components/common";
 
 export default function DonorApp() {
   return (
     <View>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>My favorite hospital is {Hospital.ASAF_HAROFE}</Text>
     </View>
   );
 }
