9a5547f5e7bed46a901480f4bbd202c560628dde
Yaron Malin <yaron1m@gmail.com>
2021-02-02 23:19:17 +0200

    Add RTL

--- packages/donor/src/index.tsx
+++ packages/donor/src/index.tsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AppNavigator from "./navigator/AppNavigator";
 import { NavigationContainer } from "@react-navigation/native";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, I18nManager } from "react-native";
+
+export function initRtl() {
+  I18nManager.allowRTL(true);
+  I18nManager.forceRTL(true);
+}
 
 export default function DonorApp() {
+  useEffect(() => initRtl(), []);
+
   return (
     <View style={styles.container}>
       <NavigationContainer>
