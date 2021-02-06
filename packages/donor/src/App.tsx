2816172774b42d8bf5db2def78c730e4f75ba657
Moris R <moris.rafol@gmail.com>
2021-02-07 00:31:43 +0200

    Donor: set the screen height to 100vh

--- packages/donor/src/App.tsx
+++ packages/donor/src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import AppRouter from "./navigation/AppRouter";
+import Div100vh from "react-div-100vh";
 
 export default function DonorApp() {
   return (
-    <div>
+    <Div100vh>
       HEADER GOES HERE
       <AppRouter />
-    </div>
+    </Div100vh>
   );
 }
