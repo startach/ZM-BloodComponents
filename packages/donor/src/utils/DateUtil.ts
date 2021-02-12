5518a534c49591113148227a6f7f1d4579b04ae3
Moris R <moris.rafol@gmail.com>
2021-02-12 12:03:01 +0200

    donor: add util functions + move dir to src

---
+++ packages/donor/src/utils/DateUtil.ts
@@ -0,0 +1,18 @@
+// tslint:disable-next-line:no-import-side-effect
+import "dayjs/locale/he";
+import dayjs from "dayjs";
+
+export function ToDateString(date: Date) {
+  return dayjs(date).format("D/M/YYYY");
+}
+
+export function ToTimeString(date: Date | string, format?: string) {
+  return dayjs(date, format).format("HH:MM");
+}
+export function ToWeekDayString(date: Date | string, format?: string) {
+  return dayjs(date, format).locale("he").format("dddd");
+}
+export function DateComparer(date1: Date, date2: Date): number {
+  // @ts-ignore
+  return date2 - date1;
+}
