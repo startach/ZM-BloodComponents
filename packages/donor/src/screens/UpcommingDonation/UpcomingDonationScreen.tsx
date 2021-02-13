5cab8229edcec020efe7b7f9291e8b29777fb9c3
Moris R <moris.rafol@gmail.com>
2021-02-14 00:11:46 +0200

    donor: add "UpcomingDonation" implementation

--- packages/donor/src/screens/UpcommingDonation/UpcomingDonationScreen.tsx
+++ packages/donor/src/screens/UpcommingDonation/UpcomingDonationScreen.tsx
@@ -1,33 +1,104 @@
 import React from "react";
-import { BookedAppointment } from "@zm-blood-components/common";
+import { BookedAppointment, LocaleUtils } from "@zm-blood-components/common";
 import styles from "./UpcommingDonationScreen.module.scss";
 import Text from "../../components/Text";
-import BookDonationHeader from "../../components/BookDonationHeader";
-import IconButton from "../../components/IconButton";
+import LastDonationDateHeader from "../../components/LastDonationDateHeader";
+import { DateFromMilliseconds } from "../../utils/DateUtil";
+import DonationInfoIcons from "../../components/DonationInfoIcons";
+import Button from "../../components/Button";
+import AwaitingYouHeader from "../../components/AwaitingYouHeader";
 
+export enum UpcomingDonationStates {
+  sameDayDonation = "sameDayDonation",
+  afterDonation = "afterDonation",
+  beforeDonation = "beforeDonation",
+}
 interface UpcomingDonationScreenProps {
-  upcomingDonations: BookedAppointment[];
+  state: UpcomingDonationStates;
+  upcomingDonation: BookedAppointment;
   lastDonation?: Date;
   firstName: string;
+  onConfirm: () => void;
   onCancel: () => void;
 }
 
 export default function UpcomingDonationScreen({
+  state,
   firstName,
   lastDonation,
   onCancel,
-  upcomingDonations,
+  upcomingDonation,
+  onConfirm,
 }: UpcomingDonationScreenProps) {
+  function renderHeader() {
+    if (state === UpcomingDonationStates.afterDonation)
+      return (
+        <LastDonationDateHeader
+          firstName={firstName}
+          lastDonation={lastDonation}
+        />
+      );
+
+    return <AwaitingYouHeader firstName={firstName} />;
+  }
+  function renderConfirmDonationTitle() {
+    if (state === UpcomingDonationStates.afterDonation)
+      return (
+        <Text className={styles.pleaseConfirmText}>
+          נא אשר שהתרומה אכן התבצעה
+        </Text>
+      );
+    return;
+  }
+  function renderConfirmButton() {
+    if (state === UpcomingDonationStates.beforeDonation) return;
+
+    const title =
+      state === UpcomingDonationStates.afterDonation
+        ? "אישור והמשך"
+        : "מאשר הגעה";
+
+    return (
+      <Button
+        title={title}
+        className={styles.registerButton}
+        onClick={onConfirm}
+      />
+    );
+  }
+  function renderCancelButton() {
+    const title =
+      state === UpcomingDonationStates.afterDonation ? "לא תרמתי" : "בטל תור";
+
+    return (
+      <div className={styles.cancelButtonContainer}>
+        <Button
+          title={title}
+          className={styles.cancelButton}
+          onClick={onCancel}
+          variant={"outlined"}
+        />
+      </div>
+    );
+  }
+
   return (
     <div className={styles.component}>
-      <BookDonationHeader firstName={firstName} lastDonation={lastDonation} />
-      <Text>{JSON.stringify(upcomingDonations)}</Text>
+      {renderHeader()}
       <main className={styles.content}>
-        <Text>נא אשר שהתרומה אכן התבצעה</Text>
-        <Text>פרטי התור</Text>
-        <div>
-          <IconButton iconSrc={} />
-        </div>
+        {renderConfirmDonationTitle()}
+
+        <Text className={styles.donationDetailsText}>פרטי התור</Text>
+
+        <DonationInfoIcons
+          donationDate={DateFromMilliseconds(
+            upcomingDonation.donationStartTimeMillis
+          )}
+          hospitalName={LocaleUtils.getHospitalName(upcomingDonation.hospital)}
+        />
+
+        {renderConfirmButton()}
+        {renderCancelButton()}
       </main>
     </div>
   );
--- packages/donor/src/screens/UpcommingDonation/UpcomingDonationScreenContainer.tsx
+++ packages/donor/src/screens/UpcommingDonation/UpcomingDonationScreenContainer.tsx
@@ -1,26 +1,35 @@
 import React, { useEffect, useState } from "react";
 import { BookedAppointment } from "@zm-blood-components/common";
-import UpcomingDonationScreen from "./UpcomingDonationScreen";
+import UpcomingDonationScreen, {
+  UpcomingDonationStates,
+} from "./UpcomingDonationScreen";
 import { useHistory } from "react-router-dom";
 import * as FirebaseFunctions from "../../firebase/FirebaseFunctions";
 
 export default function UpcomingDonationScreenContainer() {
   const history = useHistory();
   const [upcomingDonations, setUpcomingDonations] = useState<
-    BookedAppointment[]
-  >([]);
+    BookedAppointment | undefined
+  >();
 
   useEffect(() => {
-    FirebaseFunctions.getFutureAppointments().then((futureAppointments) =>
-      setUpcomingDonations(futureAppointments)
-    );
+    FirebaseFunctions.getFutureAppointments().then((futureAppointments) => {
+      setUpcomingDonations(futureAppointments[0]);
+    });
   }, []);
+
+  //TODO: ensure that a donation is always available
+  if (!upcomingDonations) return <></>;
 
   return (
     <UpcomingDonationScreen
+      state={UpcomingDonationStates.sameDayDonation}
       lastDonation={new Date(2021, 0, 13)}
-      upcomingDonations={upcomingDonations}
+      upcomingDonation={upcomingDonations}
       firstName={"יוני"}
+      onConfirm={() => {
+        console.log("donation confirmed");
+      }}
       onCancel={() => {
         console.log("Asked to cancel appointment");
         history.goBack();
