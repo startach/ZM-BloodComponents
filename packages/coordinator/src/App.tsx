edfd38af89ac4bd1a9f4a2596bda0c17551d400b
Yaron Malin <yaron1m@gmail.com>
2021-02-16 20:06:15 +0200

    Connect coordinator to workspace + extract common dependencies to root

--- packages/coordinator/src/App.tsx
+++ packages/coordinator/src/App.tsx
@@ -1,26 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { Hospital } from "@zm-blood-components/common";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [hospital] = React.useState(Hospital.TEL_HASHOMER);
+
+  return <div className="App">{hospital}</div>;
 }
 
 export default App;
