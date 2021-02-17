38ffc09167baf0ed9d167c3cf2886c6e6aff14b9
Yaron Malin <yaron1m@gmail.com>
2021-02-17 19:03:33 +0200

    Add RTL

--- packages/coordinator/src/App.tsx
+++ packages/coordinator/src/App.tsx
@@ -3,15 +3,18 @@ import "./App.css";
 import Header from "./components/Header";
 import CoordinatorRouter from "./navigation/CoordinatorRouter";
 import { BrowserRouter as Router } from "react-router-dom";
+import WithGlobalTheme from './HOCs/withGlobalTheme';
 
 function App() {
   return (
     <div className="App">
+      <WithGlobalTheme>
       <Router>
         <Header />
 
         <CoordinatorRouter />
       </Router>
+      </WithGlobalTheme>
     </div>
   );
 }
