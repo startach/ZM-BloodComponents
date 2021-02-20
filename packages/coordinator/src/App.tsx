5ce1e8cbf2908d48d41d8c482a9a475959346b2d
Yaron Malin <yaron1m@gmail.com>
2021-02-20 17:21:58 +0200

    Coordinator - add appointments flow

--- packages/coordinator/src/App.tsx
+++ packages/coordinator/src/App.tsx
@@ -3,14 +3,19 @@ import "./App.css";
 import CoordinatorRouter from "./navigation/CoordinatorRouter";
 import { BrowserRouter as Router } from "react-router-dom";
 import WithGlobalTheme from "./HOCs/withGlobalTheme";
+import { MuiPickersUtilsProvider } from "@material-ui/pickers";
+import DateFnsUtils from "@date-io/date-fns";
+import { he } from "date-fns/locale";
 
 function App() {
   return (
     <div className="App">
       <WithGlobalTheme>
-        <Router>
-          <CoordinatorRouter />
-        </Router>
+        <MuiPickersUtilsProvider utils={DateFnsUtils} locale={he}>
+          <Router>
+            <CoordinatorRouter />
+          </Router>
+        </MuiPickersUtilsProvider>
       </WithGlobalTheme>
     </div>
   );
--- packages/coordinator/src/screens/addAppointments/AddAppointmentsForm.tsx
+++ packages/coordinator/src/screens/addAppointments/AddAppointmentsForm.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import "./AppointmentForm.css";
+import styles from "./AddAppointmentsForm.module.scss";
 import Select from "../../components/Select";
 import { SelectOption } from "../../components/Select/Select";
 import { Hospital, LocaleUtils } from "@zm-blood-components/common";
-import Input from "../../components/Input";
 import Button from "../../components/Button";
+import DatePicker from "../../components/DatePicker";
+import TimePicker from "../../components/TimePicker";
 
 const hospitalOptions: SelectOption<Hospital>[] = [
   hospitalToOption(Hospital.TEL_HASHOMER),
@@ -37,29 +38,25 @@ interface AddAppointmentsFormProps {
 
 export default function AddAppointmentsForm(props: AddAppointmentsFormProps) {
   const [hospital, setHospital] = useState<Hospital>(Hospital.TEL_HASHOMER);
-  const [date, setDate] = useState("");
-  const [hour, setHour] = useState("");
+  const [date, setDate] = useState<Date | null>(getInitialDate());
   const [slots, setSlots] = useState(1);
 
-  const isButtonDisable = () => !(hospital && date && hour && slots);
+  const isButtonDisable = () => !(hospital && date && slots);
 
   const onSave = () => {
-    setDate("");
-    setHour("");
-    setSlots(1);
     props.addSlotsRequest(hospital, new Date(), slots);
   };
 
   return (
-    <div className="appointmentForm">
+    <div className={styles.appointmentForm}>
       <Select
         label={"בית חולים"}
         options={hospitalOptions}
         onChange={setHospital}
         value={hospital}
       />
-      <Input value={date} onChangeText={setDate} label={"תאריך"} />
-      <Input value={hour} onChangeText={setHour} label={"שעה"} />
+      <DatePicker value={date} onChange={setDate} label={"תאריך"} disablePast />
+      <TimePicker value={date} onChange={setDate} label={"שעה"} />
       <Select
         label={"מספר תורים"}
         options={slotOptions}
@@ -70,4 +67,15 @@ export default function AddAppointmentsForm(props: AddAppointmentsFormProps) {
       <Button onClick={onSave} title="הוספה" isDisabled={isButtonDisable()} />
     </div>
   );
+}
+
+function getInitialDate() {
+  const date = new Date();
+  date.setDate(date.getDate() + 1);
+  date.setHours(11);
+  date.setMinutes(0);
+  date.setSeconds(0);
+  date.setMilliseconds(0);
+
+  return date;
 }
