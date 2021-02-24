7db904501eaadbd048a5066ac5bcd165c54a9f3a
Moris R <moris.rafol@gmail.com>
2021-02-24 22:12:46 +0200

    coordinator: run prettier

--- packages/coordinator/src/components/ScreenWrapper/ScreenWrapper.tsx
+++ packages/coordinator/src/components/ScreenWrapper/ScreenWrapper.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import styles from "./ScreenWrapper.module.scss";
-import classname from "classnames"
-export interface ScreenWrapperProps extends React.HTMLProps<HTMLDivElement>{}
+import classname from "classnames";
+export interface ScreenWrapperProps extends React.HTMLProps<HTMLDivElement> {}
 
-function ScreenWrapper({className, ...props}: ScreenWrapperProps) {
-
-  return <div className={classname(styles.component,className)} {...props}/>;
+function ScreenWrapper({ className, ...props }: ScreenWrapperProps) {
+  return <div className={classname(styles.component, className)} {...props} />;
 }
 
 export default ScreenWrapper;
--- packages/coordinator/src/components/Select/index.tsx
+++ packages/coordinator/src/components/Select/index.tsx
@@ -1,1 +1,1 @@
-export {default, default as Select} from "./Select"
+export { default, default as Select } from "./Select";
