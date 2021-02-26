a83f226b13f8568cc938e8b8d2f5e03dd2ea57d3
Yaron Malin <yaron1m@gmail.com>
2021-02-26 19:03:36 +0200

    GetAvailableAppointments to be supported for unauthenticated users

--- packages/functions/src/testUtils/FirebaseTestUtils.ts
+++ packages/functions/src/testUtils/FirebaseTestUtils.ts
@@ -1,15 +1,5 @@
 import * as firebaseFunctionsTest from "firebase-functions-test";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyC6EDL8gMkZc3GGzGveMqWe5zvAr5DNiL4",
-  authDomain: "blood-components.firebaseapp.com",
-  databaseURL: "https://blood-components.firebaseio.com",
-  projectId: "blood-components",
-  storageBucket: "blood-components.appspot.com",
-  messagingSenderId: "388223113819",
-  appId: "1:388223113819:web:1273570a12add0fedafd7e",
-  measurementId: "G-K6NM078FWD",
-};
+import { firebaseConfig } from "@zm-blood-components/common";
 
 const serviceAccountKeyPath = "./src/testUtils/test-service-account-key.json";
 const firebaseTest = firebaseFunctionsTest(
