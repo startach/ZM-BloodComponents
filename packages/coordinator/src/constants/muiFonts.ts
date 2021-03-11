1ccda9a0034efdad540c91e799484fed00b4cdf9
Eitan <evbambly@gmail.com>
2021-03-11 16:21:12 +0200

    Rubik font added

---
+++ packages/coordinator/src/constants/muiFonts.ts
@@ -0,0 +1,13 @@
+import { BaseCSSProperties } from '@material-ui/core/styles/withStyles';
+
+
+
+export const rubik :  = {
+    fontFamily: 'Rubik',
+    fontStyle: 'normal',
+    fontDisplay: 'swap',
+    fontWeight: 400,
+    src: `
+      url(${rubikUrl}) format('woff2')
+    `,
+}
