0c8366d0a8c2e33385c8e62368cc9754fe0822d6
talwag <wagtal@gmail.com>
2021-03-16 16:39:21 +0200

    centering the spinner on the splash screen

--- packages/donor/src/screens/authentication/AuthLoadingScreen.tsx
+++ packages/donor/src/screens/authentication/AuthLoadingScreen.tsx
@@ -1,13 +1,12 @@
 import React from "react";
-// import Text from "../../components/basic/Text";
 import Spinner from "../../components/basic/Spinner";
 import Logo from "../logo/Logo";
 
 export default function AuthLoadingScreen() {
   return (
-    <div>
+    <div style={{ textAlign: "center" }}>
       <Logo style={{ width: "100%", maxWidth: "320px", height: "400px" }} />
-      <Spinner />
+      <Spinner/>
     </div>
   );
 }
