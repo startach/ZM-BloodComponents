4f28c04c2458f805155d0e3553c6dea194ac734e
Moris R <moris.rafol@gmail.com>
2021-04-11 00:05:36 +0300

    donor: delegate css color variables to js theme variables

--- packages/donor/src/constants/colors.tsx
+++ packages/donor/src/constants/colors.tsx
@@ -1,9 +1,9 @@
 // See also : src/styles/variables for CSS colors
-
+const documentStyle = getComputedStyle(document.documentElement);
 const colors = {
-  primary: "#c7007d",
-  secondary: "#347d8d",
-  info: "#f3f5f7",
+  primary: documentStyle.getPropertyValue("--color-primary").trim(),
+  secondary: documentStyle.getPropertyValue("--color-secondary").trim(),
+  info: documentStyle.getPropertyValue("--color-info").trim(),
 };
 
 export default colors;
