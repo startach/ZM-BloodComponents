fa200f6089a6aecdb6a34921827df1705a791d25
Moris R <moris.rafol@gmail.com>
2021-04-11 02:14:53 +0300

    donor: remove 100vh div component

--- packages/donor/src/App.tsx
+++ packages/donor/src/App.tsx
@@ -1,13 +1,10 @@
 import AppRouter from "./navigation/AppRouter";
-import Div100vh from "react-div-100vh";
 import WithGlobalTheme from "./HOCs/withGlobalTheme";
 
 export default function DonorApp() {
   return (
-    <Div100vh>
-      <WithGlobalTheme>
-        <AppRouter />
-      </WithGlobalTheme>
-    </Div100vh>
+    <WithGlobalTheme>
+      <AppRouter />
+    </WithGlobalTheme>
   );
 }
