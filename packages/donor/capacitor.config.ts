98b4ac0edc7e81b30e0a0508eec529892dfa5e48
Moris R <moris.rafol@gmail.com>
2021-04-18 00:32:31 +0300

    donor: replace capacitor json config file with a ts file

---
+++ packages/donor/capacitor.config.ts
@@ -0,0 +1,14 @@
+//reference https://capacitorjs.com/docs/v3/config
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.zmComponents.donor",
+  appName: "donor",
+  webDir: "./build",
+  bundledWebRuntime: false,
+  server: {
+    url: "http://localhost:3000",
+    cleartext: true,
+  },
+};
+export default config;
