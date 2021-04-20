7c2072b83a9d1d0db836dfdbe2529fa1817cb934
talwag <wagtal@gmail.com>
2021-04-20 23:32:47 +0300

    yarn test -u fix

--- packages/donor/src/__test__/TestUtils.tsx
+++ packages/donor/src/__test__/TestUtils.tsx
@@ -1,10 +1,30 @@
+import { StylesProvider } from "@material-ui/core";
 import React from "react";
 import WithGlobalTheme from "../HOCs/withGlobalTheme";
+
+// https://github.com/mui-org/material-ui/issues/9492#issuecomment-657609780
+const generateClassName = () => {
+  let counter = 0;
+  return (rule: any, styleSheet: any) =>
+    `${styleSheet.options.classNamePrefix}-${rule.key}-${counter++}`;
+};
+
+export default function WithStableMuiClassnames(props: {
+  children: React.ReactNode;
+}) {
+  return (
+    <StylesProvider generateClassName={generateClassName()}>
+      {props.children}
+    </StylesProvider>
+  );
+}
 
 export function MuiTestWrapper(props: { children: React.ReactNode }) {
   return (
     <div dir="rtl">
-      <WithGlobalTheme>{props.children}</WithGlobalTheme>
+      <WithStableMuiClassnames>
+        <WithGlobalTheme>{props.children}</WithGlobalTheme>
+      </WithStableMuiClassnames>
     </div>
   );
 }
