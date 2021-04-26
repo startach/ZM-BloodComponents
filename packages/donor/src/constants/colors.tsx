019fb62ff7bdf0d832dd4b0b96a3a350099ffb20
Moris R <moris.rafol@gmail.com>
2021-04-26 22:09:26 +0300

    donor: resolve failing tests

--- packages/donor/src/constants/colors.tsx
+++
@@ -1,9 +0,0 @@
-// See also : src/styles/variables for CSS colors
-const documentStyle = getComputedStyle(document.documentElement);
-const colors = {
-  primary: documentStyle.getPropertyValue("--color-primary").trim(),
-  secondary: documentStyle.getPropertyValue("--color-secondary").trim(),
-  info: documentStyle.getPropertyValue("--color-info").trim(),
-};
-
-export default colors;
