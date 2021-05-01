078a7e7a8214a79093ca3ab1ffe6dd606380b28a
Moris R <moris.rafol@gmail.com>
2021-05-01 23:57:25 +0300

    donor: re-wrap the entire site with "Div100vh"

--- packages/donor/src/App.tsx
+++ packages/donor/src/App.tsx
@@ -1,10 +1,13 @@
 import AppRouter from "./navigation/AppRouter";
 import WithGlobalTheme from "./HOCs/withGlobalTheme";
+import Div100vh from "react-div-100vh";
 
 export default function DonorApp() {
   return (
-    <WithGlobalTheme>
-      <AppRouter />
-    </WithGlobalTheme>
+    <Div100vh>
+      <WithGlobalTheme>
+        <AppRouter />
+      </WithGlobalTheme>
+    </Div100vh>
   );
 }
