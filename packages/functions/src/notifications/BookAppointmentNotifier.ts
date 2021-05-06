f1f9227b887b929705c54d31eb17ca68597c6acd
Yaron <yaron1m@gmail.com>
2021-05-07 00:49:44 +0300

    Split notifiers

--- packages/functions/src/notifications/BookAppointmentNotifier.ts
+++ packages/functions/src/notifications/BookAppointmentNotifier.ts
@@ -1,23 +1,24 @@
 import {
   DateUtils,
+  DbAppointment,
   DbDonor,
-  Hospital,
   LocaleUtils,
 } from "@zm-blood-components/common";
 import { sendEmailToDonor } from "./notifiers/DonorBookAppointmentNotifier";
 import { sendEmailToStaff } from "./notifiers/StaffBookAppointmentNotifier";
+import { getDonor } from "../dal/DonorDataAccessLayer";
 
 export const ZM_LOGO_URL =
   "https://firebasestorage.googleapis.com/v0/b/blood-components.appspot.com/o/Logo_ZM_he.jpg?alt=media&token=aa5e9d8c-d08e-4c80-ad7f-bfd361e36b20";
 
 export async function notifyOnAppointmentBooked(
-  donationStartTime: Date,
-  hospital: Hospital,
+  bookedAppointment: DbAppointment,
   donor: DbDonor
 ) {
+  const donationStartTime = bookedAppointment.donationStartTime.toDate();
   const dateString = DateUtils.ToDateString(donationStartTime);
   const hourString = DateUtils.ToTimeString(donationStartTime);
-  const hospitalName = LocaleUtils.getHospitalName(hospital);
+  const hospitalName = LocaleUtils.getHospitalName(bookedAppointment.hospital);
 
   await sendEmailToDonor(
     donor.email,
@@ -27,12 +28,26 @@ export async function notifyOnAppointmentBooked(
     donor.firstName
   );
 
+  const staffEmails = await getStaffRecipients(bookedAppointment);
   await sendEmailToStaff(
-    ["yaronmln@google.com"],
+    staffEmails,
     dateString,
     hourString,
     hospitalName,
     donor.firstName,
     donor.lastName
   );
+}
+
+async function getStaffRecipients(
+  bookedAppointment: DbAppointment
+): Promise<string[]> {
+  const res = ["bloodbank.ZM@gmail.com"];
+
+  const appointmentCreator = await getDonor(bookedAppointment.creatorUserId); // Because every admin is also saved as donor
+  if (appointmentCreator?.email) {
+    res.push(appointmentCreator.email);
+  }
+
+  return res;
 }
