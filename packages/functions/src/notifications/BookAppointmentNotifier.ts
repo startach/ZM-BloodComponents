bc8286fdec1134dceb9b0947b387ade3ac259b89
Yaron <yaron1m@gmail.com>
2021-05-06 23:14:02 +0300

    HTML notification emails

--- packages/functions/src/notifications/BookAppointmentNotifier.ts
+++ packages/functions/src/notifications/BookAppointmentNotifier.ts
@@ -4,82 +4,35 @@ import {
   Hospital,
   LocaleUtils,
 } from "@zm-blood-components/common";
-import {
-  addEmailToQueue,
-  EmailMessage,
-} from "../dal/EmailNotificationsDataAccessLayer";
+import { sendEmailToDonor } from "./notifiers/DonorBookAppointmentNotifier";
+import { sendEmailToStaff } from "./notifiers/StaffBookAppointmentNotifier";
+
+export const ZM_LOGO_URL =
+  "https://firebasestorage.googleapis.com/v0/b/blood-components.appspot.com/o/Logo_ZM_he.jpg?alt=media&token=aa5e9d8c-d08e-4c80-ad7f-bfd361e36b20";
 
 export async function notifyOnAppointmentBooked(
   donationStartTime: Date,
   hospital: Hospital,
   donor: DbDonor
 ) {
-  const donationTimeString =
-    DateUtils.ToTimeString(donationStartTime) +
-    " " +
-    DateUtils.ToDateString(donationStartTime);
+  const dateString = DateUtils.ToDateString(donationStartTime);
+  const hourString = DateUtils.ToTimeString(donationStartTime);
   const hospitalName = LocaleUtils.getHospitalName(hospital);
-  await sendEmailToDonor(donationTimeString, hospitalName, donor);
-  await sendEmailToCoordinator(donationTimeString, hospitalName, donor);
-}
 
-function sendEmailToDonor(
-  donationTimeString: string,
-  hospitalName: string,
-  donor: DbDonor
-) {
-  const donorName = donor.firstName;
+  await sendEmailToDonor(
+    donor.email,
+    dateString,
+    hourString,
+    hospitalName,
+    donor.firstName
+  );
 
-  const messageToDonor: EmailMessage = {
-    to: [donor.email],
-    message: {
-      subject: `אישור קביעת תרומה - ${donationTimeString}`,
-      text:
-        "שלום " +
-        donorName +
-        ",\n" +
-        "זוהי הודעת אישור לתרומה שקבעת לתאריך " +
-        donationTimeString +
-        " בבית החולים " +
-        hospitalName +
-        ".\n\n" +
-        "בברכה,\n" +
-        "צוות זכרון מנחם",
-    },
-  };
-
-  return addEmailToQueue(messageToDonor);
-}
-
-export const COORDINATOR_EMAILS: string[] = ["bloodbank.ZM@gmail.com"];
-
-function sendEmailToCoordinator(
-  donationTimeString: string,
-  hospitalName: string,
-  donor: DbDonor
-) {
-  const donorFullName = donor.firstName + " " + donor.lastName;
-
-  const messageToDonor: EmailMessage = {
-    to: COORDINATOR_EMAILS,
-    message: {
-      subject: `תרומה חדשה נקבעה לתאריך ${donationTimeString}`,
-      text:
-        "שלום,\n" +
-        "תרומה חדשה נקבעה לתאריך " +
-        donationTimeString +
-        " בבית החולים " +
-        hospitalName +
-        ".\n" +
-        "שם התורם: " +
-        donorFullName +
-        ", טלפון: " +
-        donor.phone +
-        ".\n\n" +
-        "בברכה,\n" +
-        "צוות אפליקציית זכרון מנחם",
-    },
-  };
-
-  return addEmailToQueue(messageToDonor);
+  await sendEmailToStaff(
+    ["yaronmln@google.com"],
+    dateString,
+    hourString,
+    hospitalName,
+    donor.firstName,
+    donor.lastName
+  );
 }
---
+++ packages/functions/src/notifications/notifiers/StaffBookAppointmentNotifier.ts
@@ -0,0 +1,68 @@
+import {
+  addEmailToQueue,
+  EmailMessage,
+} from "../../dal/EmailNotificationsDataAccessLayer";
+import { ZM_LOGO_URL } from "../BookAppointmentNotifier";
+
+export function sendEmailToStaff(
+  emails: string[],
+  dateString: string,
+  hourString: string,
+  hospitalName: string,
+  donorFirstName: string,
+  donorLastName: string
+) {
+  const html = getEmailContent(
+    donorFirstName,
+    donorLastName,
+    dateString,
+    hourString,
+    hospitalName
+  );
+
+  const messageToDonor: EmailMessage = {
+    to: emails,
+    message: {
+      subject: `רישום חדש לתור ${dateString + " " + hourString}`,
+      html: html,
+    },
+  };
+
+  return addEmailToQueue(messageToDonor);
+}
+
+function getEmailContent(
+  donorFirstName: string,
+  donorLastName: string,
+  dateString: string,
+  hourString: string,
+  hospitalName: string
+) {
+  return `
+<!DOCTYPE html>
+<html lang="he" dir="rtl">
+  <head>
+    <meta charset="UTF-8" />
+    <title>תודה על הרשמתך לתרומה</title>
+  </head>
+  <body style="text-align: right; direction: rtl">
+    <img src="#logo#" alt="זכרון מנחם" />
+    <h2>שלום,</h2>
+    התורם/ת
+    <span style="font-weight: bold">#שם_מלא#</span>
+    <span style="color: green; font-weight: bold">נרשם/ה</span>
+    לתור אשר יתקיים בביה"ח #בית_חולים# בתאריך #תאריך# בשעה #שעה#.
+    <br />
+    <br />
+    המשך יום טוב!
+    <br />
+    צוות אפליקציית זכרון מנחם
+  </body>
+</html>
+`
+    .replace("#logo#", ZM_LOGO_URL)
+    .replace("#שם_מלא#", donorFirstName + " " + donorLastName)
+    .replace("#בית_חולים#", hospitalName)
+    .replace("#תאריך#", dateString)
+    .replace("#שעה#", hourString);
+}
