89faa15bf9054b55d22be4ae6e8fb81415a146f9
Yaron <yaron1m@gmail.com>
2021-05-07 12:16:29 +0300

    Add names to staff recepients

--- packages/functions/src/dal/EmailNotificationsDataAccessLayer.ts
+++ packages/functions/src/dal/EmailNotificationsDataAccessLayer.ts
@@ -2,7 +2,7 @@ import * as admin from "firebase-admin";
 import { Collections } from "@zm-blood-components/common";
 
 export type EmailMessage = {
-  to: string | string[];
+  to: string;
   message: {
     subject: string;
     html: string;
@@ -10,9 +10,18 @@ export type EmailMessage = {
   appointmentId: string;
 };
 
+export type StaffRecipient = {
+  email: string;
+  name: string;
+};
+
 export function addEmailToQueue(message: EmailMessage) {
   return admin
     .firestore()
     .collection(Collections.EMAIL_NOTIFICATIONS)
     .add(message);
+}
+
+export function addEmailsToQueue(messages: EmailMessage[]) {
+  return Promise.all(messages.map(addEmailToQueue));
 }
