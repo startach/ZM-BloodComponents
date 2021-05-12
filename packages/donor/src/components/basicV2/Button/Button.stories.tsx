fbdc4f819f935d629b80981394ee75d98928cdf2
talwag <wagtal@gmail.com>
2021-05-12 17:03:58 +0300

    change button v2 logic

--- packages/donor/src/components/basicV2/Button/Button.stories.tsx
+++ packages/donor/src/components/basicV2/Button/Button.stories.tsx
@@ -8,6 +8,8 @@ export default {
 const baseArgs = {
   title: "אישור והמשך",
   onClick: () => {},
+  
+  variant: 'contained',
 };
 
 export const Main = (args: ButtonProps) => (
@@ -23,9 +25,8 @@ export const SecondaryLinePink = (args: ButtonProps) => (
 );
 SecondaryLinePink.args = {
   ...baseArgs,
-  color: "#CB007B",
-  backgroundColor: "#fff",
-  border: "1px #CB007B solid",
+  color: "secondaryPink",
+  variant: 'outlined',
 };
 
 export const SecondaryGray = (args: ButtonProps) => (
@@ -33,9 +34,8 @@ export const SecondaryGray = (args: ButtonProps) => (
 );
 SecondaryGray.args = {
   ...baseArgs,
-  color: "#707070",
-  backgroundColor: "#fff",
-  border: "1px #9CB2BD80 solid",
+  color: "secondaryGrey",
+  variant: "outlined"
 };
 
 export const Disabled = (args: ButtonProps) => (
@@ -43,9 +43,8 @@ export const Disabled = (args: ButtonProps) => (
 );
 Disabled.args = {
   ...baseArgs,
-  color: "#cbcdcf",
-  backgroundColor: "#E7ECEF80",
   isDisabled: true,
+  variant:'disabled'
 };
 
 export const DefaultState = (args: ButtonProps) => (
@@ -53,9 +52,8 @@ export const DefaultState = (args: ButtonProps) => (
 );
 DefaultState.args = {
   ...baseArgs,
-  color: "#4CAF50",
-  backgroundColor: "#fff",
-  border: "1px solid #4CAF51",
+  color: "primary",
+  variant:"outlined"
 };
 
 export const State6 = (args: ButtonProps) => (
@@ -63,8 +61,8 @@ export const State6 = (args: ButtonProps) => (
 );
 State6.args = {
   ...baseArgs,
-  color: "#fff",
-  backgroundColor: "#CB007B",
+variant:"contained",
+color: "secondaryPink"
 };
 
 export const Centered = (args: ButtonProps) => (
--- packages/donor/src/components/basicV2/Button/Button.tsx
+++ packages/donor/src/components/basicV2/Button/Button.tsx
@@ -3,11 +3,54 @@ import classnames from "classnames";
 import Spinner from "../../basic/Spinner";
 import React from "react";
 
+const getbuttonproperties = (variant: string, color: string) => {
+  let buttonTxtColor: string;
+  let border: string;
+  let buttonBackgroundColor: string;
+
+  switch (color) {  
+    case 'secondaryPink':
+      color = '#CB007B'
+      break;
+    case 'secondaryGrey':
+      color = '#707070'
+      break;
+    default:
+      color = '#4CAF50'
+      break;
+  }
+  switch (variant) {
+    case 'outlined':
+      buttonTxtColor = color;
+      buttonBackgroundColor = '#fff';
+      border = '1px solid '+color;
+      break;
+ 
+    case 'disabled':
+      buttonTxtColor ='#00000042';
+      buttonBackgroundColor = "#E7ECEF80";
+      border = 'none';
+      break;
+      
+    default:
+      buttonTxtColor = '#fff';
+      buttonBackgroundColor = color;
+      border = 'none';
+      break;
+  }
+  return {
+    buttonTxtColor,
+    buttonBackgroundColor,
+    border
+  };
+};
+
 export type ButtonProps = {
   title: string;
   onClick: () => void;
   backgroundColor?: string;
   color?: string;
+  variant?: string;
   border?: string;
   className?: string;
   startIcon?: React.ReactNode;
@@ -17,23 +60,28 @@ export type ButtonProps = {
   isCentered?: boolean;
 };
 
-const useButtonStyles = makeStyles((theme) => ({
-  buttonStyle: (props: { isCentered: boolean }) => ({
-    borderRadius: 6,
+const useButtonStyles = makeStyles({
+  root:(props:{variant:string, color:string, isCentered:boolean}) => {
+    const {buttonTxtColor,buttonBackgroundColor,border}= getbuttonproperties(props.variant, props.color);
+   return{
+     color:buttonTxtColor,
+     backgroundColor:buttonBackgroundColor,
+     border: border,
+     borderRadius: 6,
     fontSize: 18,
     width: 320,
     height: 48,
     ...(props.isCentered && { display: "block" }),
     ...(props.isCentered && { margin: "0 auto" }),
-  }),
-}));
+   }
+  }
+});
 
 export default function Button({
   onClick,
   title,
-  backgroundColor = "#4CAF51",
   color = "#fff",
-  border = "none",
+  variant = "contained",
   className,
   startIcon,
   endIcon,
@@ -41,13 +89,16 @@ export default function Button({
   isLoading = false,
   isCentered = false,
 }: ButtonProps) {
-  const classes = useButtonStyles({ isCentered });
+  const classes = useButtonStyles({
+    color,
+    variant,
+    isCentered,
+  });
 
   return (
     <MuiButton
       onClick={onClick}
-      style={{ backgroundColor: backgroundColor, color: color, border: border }}
-      className={classnames(className, classes.buttonStyle)}
+      className={classnames(className, classes.root)}
       startIcon={!isLoading && startIcon}
       endIcon={!isLoading && endIcon}
       disabled={isDisabled || isLoading}
