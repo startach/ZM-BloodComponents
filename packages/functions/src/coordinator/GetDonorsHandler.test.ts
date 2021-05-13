2226d7342369955d1c006be62448c1cfa2b3d75a
Harel Gliksman <harelglik@gmail.com>
2021-05-13 14:56:21 +0200

    review comments

--- packages/functions/src/coordinator/GetDonorsHandler.test.ts
+++ packages/functions/src/coordinator/GetDonorsHandler.test.ts
@@ -9,83 +9,100 @@ import * as Functions from "../index";
 import { deleteAdmin, setAdmin } from "../dal/AdminDataAccessLayer";
 import { expectAsyncThrows } from "../testUtils/TestUtils";
 import { sampleUser } from "../testUtils/TestSamples";
-import { deleteDonor, setDonor } from "../dal/DonorDataAccessLayer";
+import * as DonorDAL from "../dal/DonorDataAccessLayer";
+import * as GroupDAL from "../dal/GroupsDataAccessLayer";
 
 const wrapped = firebaseFunctionsTest.wrap(
   Functions[FunctionsApi.GetDonorsFunctionName]
 );
 
 const COORDINATOR_ID = "GetDonorsFunctionNameTestCoordinator";
-const DONOR_ID = "GetDonorsFunctionNameTestCoordinator";
+const DONOR_ID_1 = "GetDonorsFunctionNameTestCoordinator1";
+const DONOR_ID_2 = "GetDonorsFunctionNameTestCoordinator2";
+const GROUP_NAME_1 = "GetDonorsFunctionNameTestGroup1";
 
 const reset = async () => {
   await deleteAdmin(COORDINATOR_ID);
-  await deleteDonor(DONOR_ID);
+  await DonorDAL.deleteDonor(DONOR_ID_1);
+  await DonorDAL.deleteDonor(DONOR_ID_2);
+  const groups = await GroupDAL.getGroupIdsOfCoordinatorId(COORDINATOR_ID);
+  groups.forEach((groupId) => GroupDAL.deleteGroup(groupId));
 };
 
 beforeAll(reset);
 afterEach(reset);
-
 test("Unauthenticated user throws exception", async () => {
   const action = () => callFunction();
   await expectAsyncThrows(action, "Unauthorized");
 });
 
 test("User that is not admin throws exception", async () => {
-  const action = () => callFunction(DONOR_ID);
+  const action = () => callFunction(DONOR_ID_1);
 
   await expectAsyncThrows(
     action,
-    "User GetDonorsFunctionNameTestCoordinator is not an admin"
+    "User GetDonorsFunctionNameTestCoordinator1 is not an admin"
   );
 });
 
 test("Test users are not returned", async () => {
-  await createCoordinator();
-  await createDonor(true);
+  await createCoordinator(CoordinatorRole.SYSTEM_USER);
+  await createDonor(DONOR_ID_1, GROUP_NAME_1, true);
 
   const response = await callFunction(COORDINATOR_ID);
 
-  const testDonor = response.donors.filter((donor) => donor.id === DONOR_ID);
+  const testDonor = response.donors.filter((donor) => donor.id === DONOR_ID_1);
 
   expect(testDonor).toHaveLength(0);
 });
 
-test("All donors are returned", async () => {
-  await createCoordinator();
-  await createDonor();
+test("SYSTEM_USER gets all users", async () => {
+  await createCoordinator(CoordinatorRole.SYSTEM_USER);
+  await createDonor(DONOR_ID_1, GROUP_NAME_1);
 
   const response = await callFunction(COORDINATOR_ID);
 
-  const testDonor = response.donors.filter((donor) => donor.id === DONOR_ID);
+  const testDonor = response.donors.filter((donor) => donor.id === DONOR_ID_1);
 
   expect(testDonor).toHaveLength(1);
 });
 
-async function createCoordinator() {
+test("GROUP_COORDINATOR gets only users in group", async () => {
+  await createCoordinator(CoordinatorRole.GROUP_COORDINATOR);
+  const group = await GroupDAL.createGroup(GROUP_NAME_1, COORDINATOR_ID);
+
+  await createDonor(DONOR_ID_1, group.id);
+  await createDonor(DONOR_ID_2, "other_group_id");
+
+  const response = await callFunction(COORDINATOR_ID);
+
+  expect(response.donors).toHaveLength(1);
+});
+
+async function createCoordinator(role: CoordinatorRole) {
   const newAdmin: DbCoordinator = {
     id: COORDINATOR_ID,
-    role: CoordinatorRole.SYSTEM_USER,
+    role,
   };
 
   await setAdmin(newAdmin);
 }
 
-async function createDonor(testUser?: boolean) {
+async function createDonor(id: string, groupId: string, testUser?: boolean) {
   const donor: DbDonor = {
-    id: DONOR_ID,
     ...sampleUser,
+    id,
+    groupId,
     testUser: !!testUser,
   };
 
-  await setDonor(donor);
+  await DonorDAL.setDonor(donor);
 }
 
 function callFunction(
   userId?: string
 ): Promise<FunctionsApi.GetDonorsResponse> {
   const request: FunctionsApi.GetDonorsRequest = {};
-
   return wrapped(request, {
     auth: {
       uid: userId,
