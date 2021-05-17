301476e2df09e50c81eb40067a5444ee6245d011
Harel Gliksman <harelglik@gmail.com>
2021-05-17 18:35:32 +0200

    added alt

--- packages/donor/src/screens/contact/ContactScreen.tsx
+++ packages/donor/src/screens/contact/ContactScreen.tsx
@@ -29,6 +29,7 @@ const Header = () => {
 const ZICHRON_URL = "https://www.zichron.org";
 const ZICHRON_EMAIL = "dam@zichron.org";
 const ZICHRON_PHONE = "+972-58-710-0571";
+const ZICHRON_PHONE_TEXT = "058-710-0571";
 const ZICHRON_FACEBOOK_URL =
   "https://www.facebook.com/search/top?q=%D7%96%D7%9B%D7%A8%D7%95%D7%9F%20%D7%9E%D7%A0%D7%97%D7%9D%20zichron%20menachem";
 const ZICHRON_INSTAGRAM_URL = "https://www.instagram.com/zichronmenachem/";
@@ -39,30 +40,38 @@ const ContactContent = () => {
     <div className={styles.contentContainer}>
       <IconedContent
         iconLocation={PhoneIcon}
+        alt={"website"}
         content={<LinkTo url={ZICHRON_URL} text={ZICHRON_URL} />}
       />
       <IconedContent
         iconLocation={MailIcon}
+        alt={"email"}
         content={<MailTo email={ZICHRON_EMAIL} text={ZICHRON_EMAIL} />}
       />
       <IconedContent
         iconLocation={PhoneIcon}
-        content={<CallTo phoneNumber={ZICHRON_PHONE} text={ZICHRON_PHONE} />}
+        alt={"phone"}
+        content={
+          <CallTo phoneNumber={ZICHRON_PHONE} text={ZICHRON_PHONE_TEXT} />
+        }
       />
       <IconedContent
         iconLocation={PhoneIcon}
+        alt={"facebook"}
         content={
           <LinkTo url={ZICHRON_FACEBOOK_URL} text={"תעקבו אחרינו בפייסבוק"} />
         }
       />
       <IconedContent
         iconLocation={PhoneIcon}
+        alt={"instagram"}
         content={
           <LinkTo url={ZICHRON_INSTAGRAM_URL} text={"תעקבו אחרינו באינסטגרם"} />
         }
       />
       <IconedContent
         iconLocation={PhoneIcon}
+        alt={"youtube"}
         content={
           <LinkTo url={ZICHRON_YOUTUBE_URL} text={"תעקבו אחרינו ביוטיוב"} />
         }
@@ -74,16 +83,18 @@ const ContactContent = () => {
 type ContactLineProps = {
   content: ReactNode;
   iconLocation: string;
+  alt?: string;
 };
 
 const IconedContent: React.FC<ContactLineProps> = ({
   content,
   iconLocation,
+  alt = "",
 }) => {
   return (
     <div className={styles.contentLine}>
       <div className={styles.iconContainer}>
-        <img src={iconLocation} />
+        <img src={iconLocation} alt={alt || "icon"} />
       </div>
       {content}
     </div>
