a6ea485dce472f3227ce7a1f0d39b84756fb1a69
Eitan <evbambly@gmail.com>
2021-05-17 19:03:24 +0300

    Test

--- packages/functions/src/reports/BookedDonationInHospitalReportHandler.test.ts
+++ packages/functions/src/reports/BookedDonationInHospitalReportHandler.test.ts
@@ -19,7 +19,7 @@ import { sampleUser } from "../testUtils/TestSamples";
 import { deleteDonor, setDonor } from "../dal/DonorDataAccessLayer";
 
 const wrapped = firebaseFunctionsTest.wrap(
-  Functions.getBookedDonationsInHospital
+  Functions[FunctionsApi.GetBookedDonationsInHospitalFunctionName]
 );
 
 const COORDINATOR_ID = "BookedDonationsReportTestUser";
