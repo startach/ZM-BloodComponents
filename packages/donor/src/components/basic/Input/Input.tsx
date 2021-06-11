cde8c6b8cb8e3dbe59d9299fcb759f96cf67564d
Yaron <yaron1m@gmail.com>
2021-06-11 18:19:16 +0300

    Fix build

--- packages/donor/src/components/basic/Input/Input.tsx
+++ packages/donor/src/components/basic/Input/Input.tsx
@@ -1,5 +1,4 @@
 import TextField from "@material-ui/core/TextField";
-import { InputAdornment } from "@material-ui/core";
 import styles from "./Input.module.scss";
 import classNames from "classnames";
 
