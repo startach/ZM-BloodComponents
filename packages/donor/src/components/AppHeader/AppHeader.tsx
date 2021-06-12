2a7db4d0576c4375014cc32b52a2f250c14931af
Yaron <yaron1m@gmail.com>
2021-06-12 13:13:25 +0300

    Header V2 using MUI Closes #159

--- packages/donor/src/components/AppHeader/AppHeader.tsx
+++ packages/donor/src/components/AppHeader/AppHeader.tsx
@@ -1,97 +1,119 @@
 import firebase from "firebase/app";
 import "firebase/auth";
-import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
-import { AppBar, Typography, IconButton } from "@material-ui/core";
-import { ArrowForward } from "@material-ui/icons";
+import { AppBar, IconButton, Typography } from "@material-ui/core";
+import ArrowForward from "@material-ui/icons/ArrowForward";
 import { useHistory } from "react-router-dom";
 import { MainNavigationKeys } from "../../navigation/app/MainNavigationKeys";
-import { BurgerMenu } from "./BurgerMenu";
-import { useState } from "react";
-import { SideDrawer } from "./BurgerMenu/SideDrawer";
-import { Backdrop } from "../Backdrop";
-
+import MenuIcon from "@material-ui/icons/Menu";
+import React, { useState } from "react";
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
 import styles from "./style.module.scss";
+import AccountCircleOutlinedIcon from "@material-ui/icons/AccountCircleOutlined";
+import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import EmailOutlinedIcon from "@material-ui/icons/EmailOutlined";
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    menuButton: {
-      color: theme.palette.info.main,
-    },
-  })
-);
-
-interface AppHeaderProps {
+export interface AppHeaderProps {
   title: string;
   hasBackButton?: boolean;
   hasBurgerMenu?: boolean;
   className?: string;
 }
 
-export default function ButtonAppBar({
+export default function AppHeader({
   hasBackButton,
   title,
   hasBurgerMenu,
   className,
 }: AppHeaderProps) {
-  const classes = useStyles();
   const history = useHistory();
   const [showSideBar, setShowSideBar] = useState(false);
 
   if (!title) {
     console.error("Unknown pathname");
   }
+
+  let icon = null;
+  if (hasBurgerMenu) {
+    icon = (
+      <IconButton
+        edge="start"
+        color="inherit"
+        aria-label="menu"
+        onClick={() => setShowSideBar((previous) => !previous)}
+      >
+        <MenuIcon className={styles.rightButton} />
+      </IconButton>
+    );
+  } else if (hasBackButton) {
+    icon = (
+      <IconButton
+        edge="start"
+        color="inherit"
+        aria-label="menu"
+        onClick={() => history.goBack()}
+      >
+        <ArrowForward className={styles.rightButton} />
+      </IconButton>
+    );
+  }
+
   return (
     <AppBar position="static" color="secondary" className={className}>
       <div className={styles.headerContainer}>
-        {hasBurgerMenu && (
-          <div className={styles.headerItem}>
-            <BurgerMenu
-              onClick={() => setShowSideBar((previous) => !previous)}
-            />
-          </div>
-        )}
-        {hasBackButton && (
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-            onClick={() => history.goBack()}
-          >
-            <ArrowForward />
-          </IconButton>
-        )}
+        {icon}
         <Typography variant="h6" className={styles.title}>
           {title}
         </Typography>
       </div>
-      <div>
-        <SideDrawer
-          isOpen={showSideBar}
-          navProps={{
-            navItemsProps: [
-              {
-                title: "הפרופיל שלי",
-                onClick: () => history.push("/" + MainNavigationKeys.MyProfile),
-              },
-              {
-                title: "אודות",
-                onClick: () => history.push("/" + MainNavigationKeys.About),
-              },
-              {
-                title: "צור קשר",
-                onClick: () => history.push("/" + MainNavigationKeys.Contact),
-              },
-              {
-                title: "התנתק",
-                onClick: () => firebase.auth().signOut(),
-                class: styles.disconnect,
-              },
-            ],
-          }}
+      <Drawer
+        open={showSideBar}
+        onClose={() => setShowSideBar(false)}
+        dir={"rtl"}
+      >
+        <List>
+          <MenuItem
+            title={"הפרופיל שלי"}
+            onClick={() => history.push("/" + MainNavigationKeys.MyProfile)}
+            icon={<AccountCircleOutlinedIcon />}
+          />
+          <MenuItem
+            title={"אודות"}
+            onClick={() => history.push("/" + MainNavigationKeys.About)}
+            icon={<InfoOutlinedIcon />}
+          />
+          <MenuItem
+            title={"צור קשר"}
+            onClick={() => history.push("/" + MainNavigationKeys.Contact)}
+            icon={<EmailOutlinedIcon />}
+          />
+        </List>
+        <Divider />
+
+        <MenuItem
+          title={"התנתק"}
+          onClick={() => firebase.auth().signOut()}
+          icon={<LockOutlinedIcon />}
         />
-        {showSideBar && <Backdrop close={() => setShowSideBar(false)} />}
-      </div>
+      </Drawer>
     </AppBar>
+  );
+}
+
+function MenuItem(props: {
+  title: string;
+  onClick: () => void;
+  icon: React.ReactNode;
+}) {
+  return (
+    <ListItem button onClick={props.onClick} className={styles.listItem}>
+      <ListItemIcon>{props.icon}</ListItemIcon>
+      <ListItemText primary={props.title} />
+    </ListItem>
   );
 }
