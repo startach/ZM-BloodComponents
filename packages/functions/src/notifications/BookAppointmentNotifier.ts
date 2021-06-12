86995377317bda8066db0a3164c893f3500a8be1
Yaron <yaron1m@gmail.com>
2021-06-12 18:55:58 +0300

    Fix email logo

--- packages/functions/src/notifications/BookAppointmentNotifier.ts
+++ packages/functions/src/notifications/BookAppointmentNotifier.ts
@@ -4,8 +4,14 @@ import { sendBookingEmailToStaff } from "./notifiers/StaffBookAppointmentNotifie
 import { getAppointmentNotificationData } from "./AppointmentNotificationData";
 import { getStaffRecipients } from "./StaffEmailRecipientsCalculator";
 
-export const ZM_LOGO_URL =
+const ZM_LOGO_URL =
   "https://firebasestorage.googleapis.com/v0/b/blood-components-9ad48.appspot.com/o/blood_bank_logo.svg?alt=media&token=a823db19-51f1-4663-b7ce-c6b9cb0105d0";
+
+export const LOGO_IMAGE_TAG =
+  `<img src="#logo#" alt="זכרון מנחם" style="height: 180px"/>`.replace(
+    "#logo#",
+    ZM_LOGO_URL
+  );
 
 export async function notifyOnAppointmentBooked(
   bookedAppointment: DbAppointment,
