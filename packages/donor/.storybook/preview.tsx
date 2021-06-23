b6b3973d5ecc0b7fcd57c46a762fa0be0518dc52
Yaron <yaron1m@gmail.com>
2021-06-23 18:17:11 +0300

    Add more views to storybook

--- packages/donor/.storybook/preview.tsx
+++ packages/donor/.storybook/preview.tsx
@@ -3,12 +3,21 @@ import { StylesProvider } from "@material-ui/core";
 import WithGlobalTheme from "../src/HOCs/withGlobalTheme";
 import { Story, StoryContext } from "@storybook/react";
 import "../src/styles/index.scss";
+import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
 
 // https://github.com/mui-org/material-ui/issues/9492#issuecomment-657609780
 const generateClassName = () => {
   let counter = 0;
   return (rule: any, styleSheet: any) =>
     `${styleSheet.options.classNamePrefix}-${rule.key}-${counter++}`;
+};
+
+export const parameters = {
+  viewport: {
+    viewports: {
+      ...INITIAL_VIEWPORTS,
+    },
+  },
 };
 
 export default function WithStableMuiClassnames(props: {
