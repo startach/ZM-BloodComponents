4634de01004c7972cc590c8509f550484ea19429
Yaron <yaron1m@gmail.com>
2021-07-03 14:42:04 +0300

    New about screen

--- packages/donor/src/__test__/storybook.test.ts
+++ packages/donor/src/__test__/storybook.test.ts
@@ -1,4 +1,4 @@
-import 'jest-canvas-mock';
+import "jest-canvas-mock";
 
 import initStoryshots, {
   multiSnapshotWithOptions,
--- packages/donor/src/screens/about/AboutScreen.tsx
+++ packages/donor/src/screens/about/AboutScreen.tsx
@@ -1,43 +1,48 @@
-import Text from "../../components/basic/Text";
 import styles from "./AboutScreen.module.scss";
 import ZMScreen from "../../components/basic/ZMScreen/ZMScreen";
-import classnames from "classnames";
-import Logo from "../logo/Logo";
+import AllLogos from "./../../assets/images/all logos.svg";
 
 export default function AboutScreen() {
   return (
-    <ZMScreen hasBackButton title="אודות">
-      <Logo />
-      <Text className={classnames(styles.textBold, styles.textContainer)}>
-        על עמותת "זכרון מנחם" והאפליקציה
-      </Text>
-      <Text className={styles.textContainer}>
-        תורמי טרומבוציטים יקרים שלום רב!
-      </Text>
-      <Text className={styles.textContainer}>
-        {" "}
+    <ZMScreen hasBackButton title="אודות" fullWidth>
+      <div className={styles.imageContainer}>
+        <img src={AllLogos} alt={"logo"} className={styles.image} />
+      </div>
+
+      <div className={styles.textContainer}>
+        <div className={styles.title}>על עמותת "זכרון מנחם" והאפליקציה</div>
+        <br />
         אנו בעמותת "זכרון מנחם" עוסקים בתמיכה בילדים חולי סרטן ובני משפחותיהם
         במגוון דרכים: תמיכה נפשית ורפואית, ימי כיף, מחנות נופש, מרכזי יום,
         סדנאות, תרומות שיער ועוד (היכנסו ל
-        <a href="https://zichron.org">אתרנו</a> למידע נוסף).
-      </Text>
-      <Text className={styles.textContainer}>
+        <a href="https://zichron.org" className={styles.link}>
+          אתרנו
+        </a>{" "}
+        למידע נוסף).
+        <br />
+        <br />
         אפליקציה זו יועדה עבור תורמי הטרומבוציטים של העמותה – המגיעים לתרומה
         חד-פעמית, או באופן קבוע מספר פעמים בשנה – ועוזרים להציל חיים של אנשים.
-      </Text>
-      <Text className={styles.textContainer}>
+        <br />
+        <br />
         האפליקציה מאפשרת לכם לצפות במועדים הפנויים לתרומה בבית החולים המועדף
         עליכם, להירשם לתור, ואף לבקש הסעה ממקום מגוריכם אל בית החולים וחזרה. כי
         זה מגיע לתורמים שלנו.
-      </Text>
-      <Text className={styles.textContainer}>
+        <br />
+        <br />
         האפליקציה מפותחת ע"י מתנדבי עמותת{" "}
-        <a href="https://www.startach.org.il/">סטארט-אח</a>. מוזמנים לבקר אותנו
-        גם <a href="https://www.facebook.com/StartAchCom">בפייסבוק</a>!
-      </Text>
-      <Text className={styles.textContainer}>
+        <a href="https://www.startach.org.il/" className={styles.link}>
+          סטארט-אח
+        </a>
+        . מוזמנים לבקר אותנו גם{" "}
+        <a href="https://www.facebook.com/StartAchCom" className={styles.link}>
+          בפייסבוק
+        </a>
+        !
+        <br />
+        <br />
         תודה לכל התורמים, צוות "בנק מרכיבי הדם" של עמותת זכרון מנחם.
-      </Text>
+      </div>
     </ZMScreen>
   );
 }
