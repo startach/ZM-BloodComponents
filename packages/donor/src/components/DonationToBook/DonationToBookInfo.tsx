b463bb7b1f03dfe67045f50230ea95f20831c9ff
Yaron <yaron1m@gmail.com>
2021-07-09 13:39:11 +0300

    Export DonationToBookInfo to a component

---
+++ packages/donor/src/components/DonationToBook/DonationToBookInfo.tsx
@@ -0,0 +1,35 @@
+import styles from "./DonationToBookInfo.module.scss";
+import Calendar from "../../assets/images/AppointmentCalendar.svg";
+import { Hospital, LocaleUtils } from "@zm-blood-components/common";
+import React from "react";
+
+export interface DonationToBookInfoProps {
+  donationStartTimeMillis: number;
+  hospital: Hospital;
+}
+
+export default function DonationToBookInfo({
+  donationStartTimeMillis,
+  hospital,
+}: DonationToBookInfoProps) {
+  const donationDate = new Date(donationStartTimeMillis);
+
+  return (
+    <div className={styles.donationInfo}>
+      <img src={Calendar} alt={"Appointment"} className={styles.illustration} />
+      <div className={styles.donationInfoText}>
+        <div className={styles.infoTitle}>פרטי התור הנבחר</div>
+        <div className={styles.appointmentDetails}>
+          {donationDate.toLocaleDateString("he-He", {
+            weekday: "short",
+            month: "numeric",
+            day: "numeric",
+            hour: "numeric",
+            minute: "numeric",
+          })}
+          , {LocaleUtils.getHospitalName(hospital)}
+        </div>
+      </div>
+    </div>
+  );
+}
