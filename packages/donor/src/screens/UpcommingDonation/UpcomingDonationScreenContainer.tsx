cc9d816a2f481a132b1283271eed5ba209371ef9
Yaron <yaron1m@gmail.com>
2021-07-14 08:18:40 +0300

    Clear appointment to book when cancelling an appointment

--- packages/donor/src/screens/UpcommingDonation/UpcomingDonationScreenContainer.tsx
+++ packages/donor/src/screens/UpcommingDonation/UpcomingDonationScreenContainer.tsx
@@ -2,6 +2,7 @@ import { BookedAppointment, Donor } from "@zm-blood-components/common";
 import UpcomingDonationScreen from "./UpcomingDonationScreen";
 import * as FirebaseFunctions from "../../firebase/FirebaseFunctions";
 import { redirectToBookDonation } from "../../navigation/AppRouter";
+import { useAppointmentToBookStore } from "../../state/Providers";
 
 interface UpcomingDonationScreenContainerProps {
   user: Donor;
@@ -12,11 +13,13 @@ interface UpcomingDonationScreenContainerProps {
 export default function UpcomingDonationScreenContainer(
   props: UpcomingDonationScreenContainerProps
 ) {
+  const appointmentToBookStore = useAppointmentToBookStore();
   if (!props.bookedAppointment) {
     return redirectToBookDonation();
   }
 
   const onCancelAppointment = async () => {
+    appointmentToBookStore.clear();
     await FirebaseFunctions.cancelAppointment(props.bookedAppointment!.id);
     props.setBookedAppointment(undefined);
   };
