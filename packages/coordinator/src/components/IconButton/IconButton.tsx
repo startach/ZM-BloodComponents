6dc2d423fd82a5caf761372a7176cd4b2e639225
talwag <wagtal@gmail.com>
2021-07-24 17:45:43 +0300

    remove and revert cahnges

--- packages/coordinator/src/components/IconButton/IconButton.tsx
+++ packages/coordinator/src/components/IconButton/IconButton.tsx
@@ -1,23 +1,13 @@
-// import { useEffect, useState } from "react";
 import {
   IconButton as MaterialIcon,
   PropTypes,
-  // SvgIconTypeMap,
 } from "@material-ui/core";
-// import { OverridableComponent } from "@material-ui/core/OverridableComponent";
 import Tooltip from "@material-ui/core/Tooltip";
-// import trashIcon from "../../assets/trash.svg";
-// import userMinusIcon from "../../assets/user-minus.svg";
 
 export enum IconSize {
   small = "small",
   medium = "medium",
 }
-
-// export enum Icon {
-//   Delete = "Delete",
-//   Clear = "Clear",
-// }
 
 export interface IconButtonProps {
   size?: IconSize;
@@ -30,24 +20,10 @@ export interface IconButtonProps {
 }
 
 function IconButton({ iconUrl, tooltipText, ...props }: IconButtonProps) {
-  // let iconUrl = '';
-  // () => {
-  //   if (icon == Icon.Delete) iconUrl = trashIcon
-  //   if (icon == Icon.Clear) iconUrl = userMinusIcon
-
-  // }
-  // const [IconComponent, setIconComponent] =
-  //   useState<OverridableComponent<SvgIconTypeMap<{}, "svg">>>();
-
-  // useEffect(() => {
-  //   import("@material-ui/icons").then((icons) => setIconComponent(icons[icon]));
-  // }, [icon]);
-
-  // if (!IconComponent) return <></>;
 
   const IconElement = (
     <MaterialIcon {...props}>
-      <img src={iconUrl} alt={"Blood Bank"} />
+      <img src={iconUrl} alt={tooltipText} />
     </MaterialIcon>
   );
 
