92dd5449634c778e40cdfa030e9406d9ffc22368
talwag <wagtal@gmail.com>
2021-07-25 14:13:34 +0300

    removed iconsize and aria-label

--- packages/coordinator/src/components/IconButton/IconButton.tsx
+++ packages/coordinator/src/components/IconButton/IconButton.tsx
@@ -1,18 +1,11 @@
 import { IconButton as MaterialIcon, PropTypes } from "@material-ui/core";
 import Tooltip from "@material-ui/core/Tooltip";
 
-export enum IconSize {
-  small = "small",
-  medium = "medium",
-}
-
 export interface IconButtonProps {
-  size?: IconSize;
   color?: PropTypes.Color;
   iconUrl: string;
   onClick?: () => void;
   className?: string;
-  "aria-label"?: string;
   tooltipText?: string;
 }
 
