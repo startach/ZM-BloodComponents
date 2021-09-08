a0194f72bdd124c36ae3c650e7df93a90583103b
Eitan <evbambly@gmail.com>
2021-09-08 16:48:48 +0300

    Initial Commit

---
+++ packages/functions/src/jobs/SchemaJobs.ts
@@ -0,0 +1,36 @@
+import * as functions from "firebase-functions";
+import * as DonorDataAccessLayer from "../dal/DonorDataAccessLayer";
+
+enum Jobs {
+  DbMigrationAddAppointmentStatus = "DB_MIGRATION_ADD_APPOINTMENT_STATUS",
+}
+
+type JobResponse = {
+  message: string;
+  status: number;
+};
+
+export const jobHandler = functions.https.onRequest(async (request, res) => {
+  if (request.query?.token !== functions.config().jobHandlerToken) {
+    res.status(403);
+  }
+
+  let response: JobResponse;
+
+  switch (request.query?.jobName) {
+    case Jobs.DbMigrationAddAppointmentStatus:
+      response = await DbMigrationAddAppointmentStatus();
+      break;
+    default:
+     return res.send(404);
+  }
+
+  res.status(response.status || 400).send(response.message);
+});
+
+const DbMigrationAddAppointmentStatus = async (): Promise<JobResponse> => {
+  return {
+    message: "success!",
+    status: 200,
+  };
+};
