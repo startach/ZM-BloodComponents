f2336f38905acf8a1ad964934d8f54a03b366a89
Yaron <yaron1m@gmail.com>
2021-09-26 15:00:48 +0300

    Increase jobs timeout

--- packages/functions/src/jobs/SchemaJobs.ts
+++ packages/functions/src/jobs/SchemaJobs.ts
@@ -10,27 +10,32 @@ type JobResponse = {
   status: number;
 };
 
-export const jobHandler = functions.https.onRequest(async (request, res) => {
-  if (request.query?.token !== functions.config().job_handler_token.key) {
-    res.status(403).send();
-    return;
-  }
-
-  let response: JobResponse = {
-    message: "",
-    status: 400,
-  };
-
-  const wetRun = request.query?.wetRun === "true";
-
-  switch (request.query?.jobName) {
-    case Jobs.DbMigrationAddAppointmentStatus:
-      response = await migrateAppointmentStatus(wetRun);
-      break;
-    default:
-      res.send(404);
+export const jobHandler = functions
+  .runWith({
+    timeoutSeconds: 600,
+    memory: "1GB",
+  })
+  .https.onRequest(async (request, res) => {
+    if (request.query?.token !== functions.config().job_handler_token.key) {
+      res.status(403).send();
       return;
-  }
+    }
 
-  res.status(response.status).send(response.message);
-});
+    let response: JobResponse = {
+      message: "",
+      status: 400,
+    };
+
+    const wetRun = request.query?.wetRun === "true";
+
+    switch (request.query?.jobName) {
+      case Jobs.DbMigrationAddAppointmentStatus:
+        response = await migrateAppointmentStatus(wetRun);
+        break;
+      default:
+        res.send(404);
+        return;
+    }
+
+    res.status(response.status).send(response.message);
+  });
