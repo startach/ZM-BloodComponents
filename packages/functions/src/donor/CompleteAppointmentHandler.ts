962d3a6c1d4f711fadc635d40b01a7bb70076288
unknown <yedidya03@gmail.com>
2021-10-22 14:13:50 +0300

    one more fix

--- packages/functions/src/donor/CompleteAppointmentHandler.ts
+++ packages/functions/src/donor/CompleteAppointmentHandler.ts
@@ -4,30 +4,32 @@ import {
 } from "../dal/AppointmentDataAccessLayer";
 import { FunctionsApi } from "@zm-blood-components/common";
 import * as DbAppointmentUtils from "../utils/DbAppointmentUtils";
+import {
+  dbAppointmentToAppointmentApiEntry,
+  dbAppointmentToBookedAppointmentApiEntry,
+} from "../utils/ApiEntriesConversionUtils";
 
 export default async function (
   request: FunctionsApi.CompleteAppointmentRequest,
   callerId: string
-) {
+): Promise<FunctionsApi.CompleteAppointmentResponse> {
   const donorId = callerId;
 
   if (!request.appointmentId) {
     throw new Error("No appointment to complete");
   }
-  return {
-    appointment: await completeAppointmentFunc(
-      request.appointmentId,
-      donorId,
-      request.isNoshow
-    ),
-  };
+  return await completeAppointmentFunc(
+    request.appointmentId,
+    donorId,
+    request.isNoshow
+  );
 }
 
 export async function completeAppointmentFunc(
   appointmentId: string,
   donorId: string,
   isNoshow?: boolean
-) {
+): Promise<FunctionsApi.CompleteAppointmentResponse> {
   const appointmentToComplete = await getAppointmentsByIds([appointmentId]);
   if (appointmentToComplete.length !== 1) {
     throw new Error("Appointment not found");
@@ -43,5 +45,10 @@ export async function completeAppointmentFunc(
   const updatedAppointment =
     DbAppointmentUtils.completeArrivedFromDbAppointment(appointment, isNoshow);
 
-  return await setAppointment(updatedAppointment);
+  await setAppointment(updatedAppointment);
+
+  return {
+    completedAppointment:
+      dbAppointmentToBookedAppointmentApiEntry(updatedAppointment),
+  };
 }
