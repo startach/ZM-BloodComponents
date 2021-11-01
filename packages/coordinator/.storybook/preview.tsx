de1048b265a181abf75e93501df0019ae41cf42c
Yaron <yaron1m@gmail.com>
2021-11-01 13:19:22 +0200

    Set coordinator storybook screen sizes

--- packages/coordinator/.storybook/preview.tsx
+++ packages/coordinator/.storybook/preview.tsx
@@ -4,6 +4,10 @@ import WithGlobalTheme from "../src/HOCs/withGlobalTheme";
 import { Story, StoryContext } from "@storybook/react";
 import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
 import "../src/styles/index.scss";
+import {
+  Styles,
+  ViewportMap,
+} from "@storybook/addon-viewport/dist/ts3.9/models/Viewport";
 
 // https://github.com/mui-org/material-ui/issues/9492#issuecomment-657609780
 const generateClassName = () => {
@@ -12,11 +16,32 @@ const generateClassName = () => {
     `${styleSheet.options.classNamePrefix}-${rule.key}-${counter++}`;
 };
 
+const viewPort: ViewportMap = {
+  zmTabletSmall: {
+    name: "ZM tablet (Small)",
+    styles: {
+      width: "900px",
+      height: "1600px",
+    },
+    type: "tablet",
+  },
+  zmTabletBig: {
+    name: "ZM tablet (Big)",
+    styles: {
+      width: "1200px",
+      height: "2000px",
+    },
+    type: "tablet",
+  },
+};
+
 export const parameters = {
   viewport: {
     viewports: {
+      ...viewPort,
       ...INITIAL_VIEWPORTS,
     },
+    defaultViewport: "zmTabletSmall",
   },
 };
 
