2f7c9f935ec56c9da13de61773ffcedc5b95e48a
Yaron <yaron1m@gmail.com>
2021-12-06 21:20:33 +0200

    Fix build

--- packages/donor/src/screens/about/DonationProcessScreenContainer.tsx
+++ packages/donor/src/screens/about/DonationProcessScreenContainer.tsx
@@ -1,17 +1,8 @@
-import { MainNavigationKeys } from "../../navigation/app/MainNavigationKeys";
 import { useHistory } from "react-router-dom";
 import DonationProcessScreen from "./DonationProcessScreen";
 
-interface DonationProcessScreenContainerProps {}
-
-export default function DonationProcessScreenContainer(
-  props: DonationProcessScreenContainerProps
-) {
+export default function DonationProcessScreenContainer() {
   const history = useHistory();
 
-  return (
-    <DonationProcessScreen
-      onContact={() => history.replace("/" + MainNavigationKeys.Contact)}
-    />
-  );
+  return <DonationProcessScreen />;
 }
