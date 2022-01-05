ef352981f264a7e0212a9b30b272f076ca550442
Yaron <yaron1m@gmail.com>
2022-01-05 11:15:33 +0200

    Change to 20 seconds

--- packages/donor/src/screens/UpcommingDonation/SameDayDonationUtil.ts
+++ packages/donor/src/screens/UpcommingDonation/SameDayDonationUtil.ts
@@ -10,11 +10,11 @@ export function shouldDisplaySameDayDonationPopup(
     return false;
   }
 
-  // If appointment was booked more than 10s ago
+  // If appointment was booked more than 20s ago
   // Without this, a user could schedule on Sunday an appointment starting on Monday,
   // and then re-open the app on Monday and see the popup.
   // Here we verify that the appointment was booked very recently
-  if (now.getTime() - bookingTime.getTime() > 10_000) {
+  if (now.getTime() - bookingTime.getTime() > 20_000) {
     return false;
   }
 
