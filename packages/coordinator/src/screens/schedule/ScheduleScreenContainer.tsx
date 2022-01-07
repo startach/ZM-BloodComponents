382471156b2a2e21add4760c578a706e9d44cc53
Yaron <yaron1m@gmail.com>
2022-01-07 17:42:45 +0200

    Change schedule spinner

--- packages/coordinator/src/screens/schedule/ScheduleScreenContainer.tsx
+++ packages/coordinator/src/screens/schedule/ScheduleScreenContainer.tsx
@@ -19,6 +19,7 @@ export default function ScheduleScreenContainer(
   const [hospital, setHospital] = useState(props.availableHospitals[0]);
 
   useEffect(() => {
+    setDays([]);
     fetchScheduleAppointments(hospital, selectedDayInWeek).then((days) =>
       setDays(days)
     );
