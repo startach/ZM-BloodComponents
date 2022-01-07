60fe2eab560c421b1a40840f5c6d379c287bdaf8
Yaron <yaron1m@gmail.com>
2022-01-07 13:33:37 +0200

    Final screen

--- packages/coordinator/src/screens/schedule/ScheduleScreen.stories.tsx
+++ packages/coordinator/src/screens/schedule/ScheduleScreen.stories.tsx
@@ -1,9 +1,9 @@
-import ScheduleScreen, {ScheduleScreenProps} from "./ScheduleScreen";
-import _ from 'lodash';
-import {action} from '@storybook/addon-actions';
-import {ScheduleCell, ScheduleDay} from '../../utils/types';
-import {ScheduleWeekComponentProps} from '../../components/Schedule/ScheduleWeek/ScheduleWeekComponent';
-import {Hospital} from '@zm-blood-components/common';
+import ScheduleScreen, { ScheduleScreenProps } from "./ScheduleScreen";
+import _ from "lodash";
+import { action } from "@storybook/addon-actions";
+import { ScheduleCell, ScheduleDay } from "../../utils/types";
+import { ScheduleWeekComponentProps } from "../../components/Schedule/ScheduleWeek/ScheduleWeekComponent";
+import { Hospital } from "@zm-blood-components/common";
 
 export default {
   component: ScheduleScreen,
@@ -11,7 +11,7 @@ export default {
   parameters: { layout: "fullscreen" },
 };
 
-const weekStartMillis = 1641074400000; // January 2, 2022 0:00:00 GMT+02:00
+const weekStartMillis = 1642284000000; // January 16, 2022 0:00:00 GMT+02:00
 const millisInHour = 60 * 60 * 1000;
 const daysOffset = _.range(6);
 const onClick = action("onClickCell");
@@ -37,14 +37,17 @@ const days = daysOffset.map<ScheduleDay>((dayOffset) => {
 
 const props: ScheduleScreenProps = {
   days,
-  onNextWeek:  action( "onNextWeek"),
-  oPreviousWeek:  action( "oPreviousWeek"),
-  onAddAppointment:  action( "onAddAppointment"),
+  onNextWeek: action("onNextWeek"),
+  oPreviousWeek: action("oPreviousWeek"),
+  onAddAppointment: action("onAddAppointment"),
   availableHospitals: [
     Hospital.HADASA_EIN_KEREM,
     Hospital.SOROKA,
     Hospital.TEL_HASHOMER,
-  ],};
+  ],
+};
 
-export const Default = (args: ScheduleScreenProps) => <ScheduleScreen {...args} />;
+export const Default = (args: ScheduleScreenProps) => (
+  <ScheduleScreen {...args} />
+);
 Default.args = props;
--- packages/coordinator/src/screens/schedule/ScheduleScreen.tsx
+++ packages/coordinator/src/screens/schedule/ScheduleScreen.tsx
@@ -1,44 +1,43 @@
-import styles from "./ScheduleScreen.module.scss";
-import CoordinatorHeader from "../../components/CoordinatorHeader";
-import {HeaderVariant} from '../../components/CoordinatorHeader/CoordinatorHeader';
-import {Hospital} from '@zm-blood-components/common';
-import Fab from '../../components/Fab';
-import AddIcon from '@material-ui/icons/Add';
-import {ScheduleDay} from '../../utils/types';
-import SchedulePicker from '../../components/Schedule/ScheduleWeekPicker';
-import {useState} from 'react';
-import ScheduleWeekComponent from '../../components/Schedule/ScheduleWeek';
+import { HeaderVariant } from "../../components/CoordinatorHeader/CoordinatorHeader";
+import { Hospital } from "@zm-blood-components/common";
+import { ScheduleDay } from "../../utils/types";
+import SchedulePicker from "../../components/Schedule/ScheduleWeekPicker";
+import { useState } from "react";
+import ScheduleWeekComponent from "../../components/Schedule/ScheduleWeek";
+import CoordinatorScreen from "../../components/CoordinatorScreen";
 
 export interface ScheduleScreenProps {
   days: ScheduleDay[];
-  onNextWeek: ()=>void;
-  oPreviousWeek: ()=>void;
-  onAddAppointment: ()=>void;
+  onNextWeek: () => void;
+  oPreviousWeek: () => void;
+  onAddAppointment: () => void;
   availableHospitals: Hospital[];
 }
 
 export default function ScheduleScreen(props: ScheduleScreenProps) {
-  const [hospital, setHospital] = useState(props.availableHospitals[0])
+  const [hospital, setHospital] = useState(props.availableHospitals[0]);
 
   return (
-    <div className={styles.screen}>
-      <CoordinatorHeader title={"ניהול תורים"} hasBurgerMenu hasNotificationsIcon variant={HeaderVariant.INFO}/>
-
-      <SchedulePicker
-          weekStartTime={props.days[0].cells[0].cellStartTime}
-          onNext={props.onNextWeek}
-          onPrevious={props.oPreviousWeek}
-          hospital={hospital}
-          setSelectedHospital={setHospital}
-          availableHospitals={props.availableHospitals}
-      />
-
-      <ScheduleWeekComponent days={props.days}/>
-
-      <Fab onClick={props.onAddAppointment}>
-        <AddIcon />
-        <div style={{ marginRight: 5, fontSize: 16 }}>הוסף תור</div>
-      </Fab>
-    </div>
+    <CoordinatorScreen
+      showFab
+      headerProps={{
+        title: "ניהול תורים",
+        variant: HeaderVariant.INFO,
+        hasBurgerMenu: true,
+        hasNotificationsIcon: true,
+        stickyComponent: (
+          <SchedulePicker
+            weekStartTime={props.days[0].cells[0].cellStartTime}
+            onNext={props.onNextWeek}
+            onPrevious={props.oPreviousWeek}
+            hospital={hospital}
+            setSelectedHospital={setHospital}
+            availableHospitals={props.availableHospitals}
+          />
+        ),
+      }}
+    >
+      <ScheduleWeekComponent days={props.days} />
+    </CoordinatorScreen>
   );
 }
