580d5e8a71c3a8936839c2e22fdc01866803cacd
unknown <yedidya03@gmail.com>
2022-01-08 22:54:28 +0200

    adding CoordinatorScreen

--- packages/coordinator/src/screens/AddAppointmentScreen/AddAppointmentScreen.tsx
+++ packages/coordinator/src/screens/AddAppointmentScreen/AddAppointmentScreen.tsx
@@ -9,6 +9,9 @@ import { SelectOption } from "@zm-blood-components/common";
 import classnames from "classnames";
 import { range } from "lodash";
 
+import CoordinatorScreen from "../../components/CoordinatorScreen";
+import { HeaderVariant } from "../../components/CoordinatorHeader/CoordinatorHeader";
+
 export interface AddAppointmentScreenProps {
   onSubmit: (date: Date, hour: Date, numberOfPlaces: number) => void;
 }
@@ -29,72 +32,69 @@ export default function AddAppointmentScreen(props: AddAppointmentScreenProps) {
   const navigate = useNavigate();
 
   return (
-    <div className={styles.addAppoinmentScreen}>
-      <div className={styles.cardContainer}>
-        <div className={styles.title}>הוספת תור יחיד</div>
+    <CoordinatorScreen
+      className={styles.addApointmentSceenContent}
+      headerProps={{
+        title: "הוספת תור יחיד",
+        variant: HeaderVariant.SECONDARY,
+        hasBackButton: true,
+      }}>
+      <div className={styles.addAppoinmentScreenCardContainer}>
+        <div className={styles.cardContainer}>
+          <div className={styles.addApponimentSubtitle}>
+            <div className={styles.subtitleText}>זמני תור</div>
+          </div>
+          <DatePicker
+            className={styles.inputField}
+            value={date}
+            onChange={setDate}
+            label={"תאריך"}
+            disablePast
+          />
+          <TimePicker
+            className={styles.inputField}
+            value={hour}
+            onChange={setHour}
+            label={"שעה"}
+          />
+        </div>
+        <div className={styles.cardContainer}>
+          <div className={styles.addApponimentSubtitle}>
+            <div className={styles.subtitleText}>מאפייני תור</div>
+          </div>
+          <Select
+            containerClassName={styles.inputField}
+            id={"donations_count"}
+            label={"מספר עמדות"}
+            options={slotOptions}
+            onChange={setSlots}
+            value={slots}
+          />
+        </div>
         <div
-          className={styles.appointmentScreenXButton}
-          onClick={() => {
-            navigate(-1);
-          }}
+          className={classnames(
+            styles.cardContainer,
+            styles.appointmentsScreenButtons
+          )}
         >
-          X
+          <Button
+            className={styles.inputField}
+            onClick={() => {
+              if (date !== null && hour !== null) {
+                props.onSubmit(date, hour, slots);
+              }
+            }}
+            isDisabled={date === null || hour === null || slots === 0}
+            title="אשר והמשך"
+          />
+          <Button
+            className={styles.inputField}
+            onClick={() => navigate(-1)}
+            title="ביטול"
+            variant={ButtonVariant.outlined}
+          />
         </div>
       </div>
-      <div className={styles.cardContainer}>
-        <div className={styles.addApponimentSubtitle}>
-          <div className={styles.subtitleText}>זמני תור</div>
-        </div>
-        <DatePicker
-          className={styles.inputField}
-          value={date}
-          onChange={setDate}
-          label={"תאריך"}
-          disablePast
-        />
-        <TimePicker
-          className={styles.inputField}
-          value={hour}
-          onChange={setHour}
-          label={"שעה"}
-        />
-      </div>
-      <div className={styles.cardContainer}>
-        <div className={styles.addApponimentSubtitle}>
-          <div className={styles.subtitleText}>מאפייני תור</div>
-        </div>
-        <Select
-          containerClassName={styles.inputField}
-          id={"donations_count"}
-          label={"מספר עמדות"}
-          options={slotOptions}
-          onChange={setSlots}
-          value={slots}
-        />
-      </div>
-      <div
-        className={classnames(
-          styles.cardContainer,
-          styles.appointmentsScreenButtons
-        )}
-      >
-        <Button
-          className={styles.inputField}
-          onClick={() => {
-            if (date !== null && hour !== null) {
-              props.onSubmit(date, hour, slots);
-            }
-          }}
-          isDisabled={date === null || hour === null || slots === 0}
-          title="אשר והמשך"
-        />
-        <Button
-          className={styles.inputField}
-          onClick={() => navigate(-1)}
-          title="ביטול"
-          variant={ButtonVariant.outlined}
-        />
-      </div>
-    </div>
+    </CoordinatorScreen>
   );
 }
