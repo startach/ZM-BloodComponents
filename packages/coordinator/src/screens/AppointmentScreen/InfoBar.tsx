e960fc65c1b23cf64ec199039b5151072f0f42c9
lioz steuer <liozsteuer@gmail.com>
2022-01-10 17:12:49 +0200

    added info bar

---
+++ packages/coordinator/src/screens/AppointmentScreen/InfoBar.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import styles from "./InfoBar.module.scss.module.scss";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+
+export type InfoBarProps = {
+  title: string;
+  value: string;
+  icon: React.ReactNode;
+};
+
+export default function InfoBar({
+                                  title,
+                                  value,
+                                  icon
+                                }: InfoBarProps) {
+
+  return (
+    <div
+      className={styles.infoBar}
+      >
+      <ListItemIcon>{icon}</ListItemIcon>
+      <div
+        className={styles.titleContent}>
+        <div>{title}</div>
+        <div>{value}</div>
+      </div>
+      </div>
+  );
+}
+
