6cdbcbfb8fc25a71462c639b0b3fa8d799778745
lioz steuer <liozsteuer@gmail.com>
2022-01-11 20:39:49 +0200

    changed everything

--- packages/coordinator/src/screens/bookedDonation/BookedAppointmentScreen.tsx
+++ packages/coordinator/src/screens/bookedDonation/BookedAppointmentScreen.tsx
@@ -5,6 +5,11 @@ import {
 } from "@zm-blood-components/common";
 import { HeaderVariant } from "../../components/CoordinatorHeader/CoordinatorHeader";
 import CoordinatorScreen from "../../components/CoordinatorScreen";
+import InfoBar from "./InfoBar";
+import { ReactComponent as Phone } from "../../assets/icons/phone.svg";
+import { ReactComponent as Bloodtype } from "../../assets/icons/bloodtype.svg";
+import { ReactComponent as Calender } from "../../assets/icons/calender.svg";
+import styles from "./BookedAppointmentScreen.module.scss";
 
 export type BookedAppointmentScreenProps = {
   appointment?: BookedDonationWithDonorDetails;
@@ -23,6 +28,9 @@ export default function BookedAppointmentScreen(
   const time = DateUtils.ToDateString(
     props.appointment.donationStartTimeMillis
   );
+  const bookingTime = DateUtils.ToDateString(
+    props.appointment.bookingTimeMillis
+  );
   return (
     <CoordinatorScreen
       headerProps={{
@@ -33,7 +41,18 @@ export default function BookedAppointmentScreen(
         stickyComponent: <div>{props.appointment.firstName}</div>,
       }}
     >
-      {JSON.stringify(props)}
+      <div className={styles.details}>
+        פרטי התורם
+      </div>
+      <InfoBar title={"מספר טלפון"} value={<a href={`tel: ${props.appointment.phone}`}>{props.appointment.phone}</a>} icon={<Phone/>}/>
+      <InfoBar title={"סוג דם"} value={getValueInDiv(props.appointment.bloodType)} icon={<Bloodtype/>}/>
+      <InfoBar title={"נקבע בתאריך"} value={getValueInDiv(bookingTime)} icon={<Calender/>}/>
     </CoordinatorScreen>
   );
+}
+
+function getValueInDiv(value:string){
+  return (
+    <div className={styles.values}>{value}</div>
+  )
 }
--- packages/coordinator/src/screens/bookedDonation/InfoBar.tsx
+++ packages/coordinator/src/screens/bookedDonation/InfoBar.tsx
@@ -4,17 +4,19 @@ import ListItemIcon from "@material-ui/core/ListItemIcon";
 
 export type InfoBarProps = {
   title: string;
-  value: string;
+  value: React.ReactNode;
   icon: React.ReactNode;
 };
 
-export default function InfoBar({ title, value, icon }: InfoBarProps) {
+export default function InfoBar({ title, value, icon}: InfoBarProps) {
   return (
     <div className={styles.infoBar}>
-      <ListItemIcon>{icon}</ListItemIcon>
+      <div className={styles.icon}>
+      {icon}
+      </div>
       <div className={styles.titleContent}>
         <div>{title}</div>
-        <div>{value}</div>
+        {value}
       </div>
     </div>
   );
