2d1c71474c513a338f375086ede4c4af940fbdca
Yaron <yaron1m@gmail.com>
2022-01-22 14:34:23 +0200

    Delete (most) old coordinator screens

--- packages/coordinator/src/components/V2/MuiIconButton/index.ts
+++
@@ -1,2 +0,0 @@
-export { default, default as MuiIconButton } from "./MuiIconButton";
-export * from "./MuiIconButton";
--- packages/coordinator/src/screens/manageAppointmentsScreenOld/CopyAppointmentDetailsHelper.test.ts
+++ packages/coordinator/src/screens/manageAppointmentsScreenOld/CopyAppointmentDetailsHelper.test.ts
@@ -1,44 +1,18 @@
-import { getAppointmentCopyString } from "./CopyAppointmentDetailsHelper";
-import { Appointment } from "../../utils/types";
+import { getAppointmentCopyStringContent } from "./CopyAppointmentDetailsHelper";
 
 const exampleName = "name";
 const examplePhoneNumber = "000";
 const exampleDonationTime = 946677600000;
 const exampleDonationTimeFormat = "01/01/2000 00:00";
 
-let exmpleAppointment: Appointment = {
-  appointmentId: "abcde",
-  booked: true,
-  isPastAppointment: false,
-  donorName: exampleName,
-  donationStartTimeMillis: exampleDonationTime,
-  donorPhoneNumber: examplePhoneNumber,
-};
-
 test("test copy string provider with time value", () => {
-  const functionResult: string = getAppointmentCopyString(exmpleAppointment);
+  const functionResult: string = getAppointmentCopyStringContent(
+    exampleName,
+    examplePhoneNumber,
+    exampleDonationTime
+  );
 
   expect(functionResult).toEqual(
     `${exampleName}, ${examplePhoneNumber}, ${exampleDonationTimeFormat}`
   );
 });
-
-test.each([
-  ["", "", undefined],
-  ["", undefined, ""],
-  [undefined, "", ""],
-])(
-  "test copy string provider with missing values",
-  (donationStartTimeMillis, donorName, donorPhoneNumber) => {
-    exmpleAppointment = {
-      ...exmpleAppointment,
-      donationStartTimeMillis: donationStartTimeMillis,
-      donorName: donorName,
-      donorPhoneNumber: donorPhoneNumber,
-    };
-
-    const functionResult: string = getAppointmentCopyString(exmpleAppointment);
-
-    expect(functionResult).toEqual("");
-  }
-);
