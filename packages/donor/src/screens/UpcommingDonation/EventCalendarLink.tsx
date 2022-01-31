780afd70025209bda17794e1b3e6701b3af86400
lioz steuer <liozsteuer@gmail.com>
2022-01-31 11:31:05 +0200

    added ios calender event

--- packages/donor/src/screens/UpcommingDonation/EventCalendarLink.tsx
+++ packages/donor/src/screens/UpcommingDonation/EventCalendarLink.tsx
@@ -62,6 +62,33 @@ export default function EventCalendarLink({
     window.open(googleEventUrlWithParams, "_blank");
   };
 
+  const addToIOSCalendar = () => {
+    const calendarUrl = [
+      "BEGIN:VCALENDAR",
+      "VERSION:2.0",
+      "BEGIN:VEVENT",
+      "URL:" + document.URL,
+      "DTSTART:" + CleanIsoFormatDate(eventDateStart),
+      "DTEND:" + CleanIsoFormatDate(eventDateEnd),
+      "SUMMARY:" + "תרומת טרומבוציטים",
+      "DESCRIPTION:" + eventDescription,
+      "LOCATION:" + eventLocation,
+      "END:VEVENT",
+      "END:VCALENDAR",
+    ].join("\n");
+
+    const filename = "download.ics";
+    const blob = new Blob([calendarUrl], {
+      type: "text/calendar;charset=utf-8",
+    });
+    const link = document.createElement("a");
+    link.href = window.URL.createObjectURL(blob);
+    link.setAttribute("download", filename);
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <div>
       {DeviceUtils.getMobileOperatingSystem() === "android" ? (
@@ -69,10 +96,8 @@ export default function EventCalendarLink({
           הוספה ליומן
         </div>
       ) : (
-        <div className={styles.link}>
-          <ICalendarLink event={getAppleCalendarEvent()}>
-            הוספה ליומן
-          </ICalendarLink>
+        <div className={styles.link} onClick={addToIOSCalendar}>
+          הוספה ליומן
         </div>
       )}
     </div>
