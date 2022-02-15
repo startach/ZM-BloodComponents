1c357b8db633d7a60e0b3dda8d0853db1cb6776a
Eitan <evbambly@gmail.com>
2022-02-15 23:24:00 +0200

    PR Fix

--- packages/donor/src/Analytics.tsx
+++ packages/donor/src/Analytics.tsx
@@ -5,7 +5,6 @@ export function reportScreen(path: string) {
   if (!firebaseAnalytics) {
     return;
   }
-  console.log("screen" + path);
   logEvent(firebaseAnalytics, "screen_view", {
     firebase_screen: path,
     firebase_screen_class: "donor_screen",
@@ -16,7 +15,6 @@ export function reportClick(path: string, buttonName: string) {
   if (!firebaseAnalytics) {
     return;
   }
-  console.log(`click${path}/${buttonName}`);
   logEvent(firebaseAnalytics, `button_click`, {
     firebase_screen: path,
     firebase_screen_class: "donor_screen",
