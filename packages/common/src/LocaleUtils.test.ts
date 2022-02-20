d602894f219db46cdc89fd6d1def50d33f4e06b5
Eitan <evbambly@gmail.com>
2022-02-20 14:58:28 +0200

    Added Tests

--- packages/common/src/LocaleUtils.test.ts
+++ packages/common/src/LocaleUtils.test.ts
@@ -1,5 +1,10 @@
 import { BloodType, Hospital } from "./types";
-import { getBloodTypeTranslation, getHospitalName } from "./LocaleUtils";
+import {
+  getBloodTypeTranslation,
+  getHospitalName,
+  getValidationErrorTranslation,
+} from "./LocaleUtils";
+import { PersonalDetailsValidation } from "./Validation";
 
 test("All hospitals have names", () => {
   Object.values(Hospital).map((hospital) => {
@@ -10,5 +15,13 @@ test("All hospitals have names", () => {
 test("All blood types have names", () => {
   Object.values(BloodType).map((bloodType) => {
     expect(getBloodTypeTranslation(bloodType)).not.toEqual(bloodType);
+  });
+});
+
+test("All personal detail validations have names", () => {
+  Object.values(PersonalDetailsValidation).map((personalDetailValidation) => {
+    expect(getValidationErrorTranslation(personalDetailValidation)).not.toEqual(
+      personalDetailValidation
+    );
   });
 });
