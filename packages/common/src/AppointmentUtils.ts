89e4f1cd3fa7882b31088a07cee5903cf436197d
Rotem <rotem.barsela95@gmail.com>
2022-08-01 17:49:20 +0300

    minor changes

--- packages/common/src/AppointmentUtils.ts
+++ packages/common/src/AppointmentUtils.ts
@@ -5,18 +5,24 @@ export function isManualDonor(donorId: string) {
   return donorId === MANUAL_DONOR_ID;
 }
 
+// Days between the donation date and the current day date (1 = 24 hours, 2 = 48 hours, ...)
+const DAYS_LIMIT_BETWEEN_DATES = 2;
+
 export function isAppointmentClose({
-  ...bookedAppointment
+  donationStartTimeMillis,
+  bookingTimeMillis,
 }: BookedAppointment) {
-  const DAYS_LIMIT = 2; // Days between the donation date and the current day date (1 = 24 hours, 2 = 48 hours, ...)
   const currentDayTime = new Date();
   const numberOfDaysBetweenDates = DateUtils.getNumberOfDaysBetweenDates(
-    bookedAppointment.donationStartTimeMillis,
+    donationStartTimeMillis,
     currentDayTime.getTime()
   );
   const currentDayMillis = currentDayTime.getTime();
   // Checks if the donation is in the time frame
-  if (numberOfDaysBetweenDates > DAYS_LIMIT || numberOfDaysBetweenDates < 0) {
+  if (
+    numberOfDaysBetweenDates > DAYS_LIMIT_BETWEEN_DATES ||
+    numberOfDaysBetweenDates < 0
+  ) {
     return false;
   }
 
@@ -24,7 +30,7 @@ export function isAppointmentClose({
   // Without this, a user could schedule on Sunday an appointment starting on Monday,
   // and then re-open the app on Monday and see the popup.
   // Here we verify that the appointment was booked very recently
-  if (currentDayMillis - bookedAppointment.bookingTimeMillis > 20_000) {
+  if (currentDayMillis - bookingTimeMillis > 20_000) {
     return false;
   }
 
